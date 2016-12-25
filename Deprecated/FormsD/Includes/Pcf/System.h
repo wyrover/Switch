fa7d85ae2adfc8bdd2580191addc8c3953f678ba
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2016-12-26 00:30:14 +0100

    Remove Deprecated folder and files

--- Deprecated/FormsD/Includes/Pcf/System.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "System/ComponentModel.h"
-#include "System/Media.h"
-#include "System/Windows.h"
-WinMain
--- Deprecated/FormsD/Includes/Pcf/System/Media.h
+++
@@ -1,6 +0,0 @@
-/// @file
-/// @brief Contains Pcf::System::Media namespace.
-#pragma once
-
-#include "Media/SystemSound.h"
-#include "Media/SystemSounds.h"
