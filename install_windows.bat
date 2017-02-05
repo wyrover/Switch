aaed52e237661682fcb8685c058ad4f2bdc281bb
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-02-05 10:48:07 +0100

    fix install on windows (part 3)

--- install_windows.bat
+++ install_windows.bat
@@ -7,4 +7,19 @@ mkdir c:\usr\local
 cd Build\ThirdParties
 cmake -G "Visual Studio 14 2015 Win64" -D CMAKE_INSTALL_PREFIX:STRING="C:/usr/local" ../../ThirdParties
 "%VS140COMNTOOLS%\..\IDE\devenv" "ThirdParties.sln" /project install /build Debug
+"%VS140COMNTOOLS%\..\IDE\devenv" "ThirdParties.sln" /project install /build Release
+
+cd..
+cmake -G "Visual Studio 14 2015 Win64" -D CMAKE_INSTALL_PREFIX:STRING="C:/usr/local" ..
+"%VS140COMNTOOLS%\..\IDE\devenv" "Pcf.sln" /project documentation /build Debug
+start Help/html/index.html
+"%VS140COMNTOOLS%\..\IDE\devenv" "Pcf.sln" /project install /build Debug
+"%VS140COMNTOOLS%\..\IDE\devenv" "Pcf.sln" /project install /build Release
+
+cd Build\Examples
+cd Build\ThirdParties
+cmake -G "Visual Studio 14 2015 Win64" -D CMAKE_INSTALL_PREFIX:STRING="C:/usr/local" ../../Examples
+"%VS140COMNTOOLS%\..\IDE\devenv" "Examples.sln" /project ALL_BUILD /build Release
+start Examples.xcodeproj
 cd ..\..
+
