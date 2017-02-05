2cd0bf86bb6d6eaacb55007dc54422ca8237fd49
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-02-05 10:27:34 +0100

    fix install on windows (part 2)

--- install_windows.bat
+++ install_windows.bat
@@ -2,9 +2,9 @@
 del /q /s Build
 mkdir Build\Examples
 mkdir Build\ThirdParties
-mkdir usr/local
+mkdir c:\usr\local
 
 cd Build\ThirdParties
-cmake -G "Visual Studio 14 2015 Win64" -D CMAKE_INSTALL_PREFIX:STRING:"C=/usr/local" ../../ThirdParties
+cmake -G "Visual Studio 14 2015 Win64" -D CMAKE_INSTALL_PREFIX:STRING="C:/usr/local" ../../ThirdParties
 "%VS140COMNTOOLS%\..\IDE\devenv" "ThirdParties.sln" /project install /build Debug
 cd ..\..
