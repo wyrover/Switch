a04f7915de89dd442d8bcb00315255758249ba55
yfi <yfi@evs.com>
2017-02-07 16:53:23 +0100

    install

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
-$OSTYPE=`uname -a`
-case "$OSTYPE" in
-  darwin*) ./install_macosx_xcode.sh;;
-  *Ubuntu*) ./install_ubuntu_gcc.sh;;
-  *CentOs*) ./install_centos_gcc.sh;;
-  *) ./install_linux_gcc.sh;;
-esac
+OSTYPE=`uname -a`
+if [[ $OSTYPE == *"darwin"* ]]; then
+  echo "Install on Mac os X"
+  #./install_macosx_xcode.sh
+else
+  OSTYPE=`lsb_release -si`
+  case "$OSTYPE" in
+    *"Ubuntu"*) echo "Install on Ubuntu";;
+    *"CentOS"*) echo "Install on CentOs";;
+    *) echo "Install on Generic linux";;
+  esac
+fi
+
+#    *"Ubuntu"*) ./install_ubuntu_gcc.sh;;
+#    *"CentOS"*) ./install_centos_gcc.sh;;
+#    *) ./install_linux_gcc.sh;;
+
