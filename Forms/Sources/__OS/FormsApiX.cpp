3f19a5a901f3f270a3c66bf51f3396ed93e7f64a
yfiumefreddo <y.fiumefreddo@gmail.com>
2017-03-16 23:07:24 +0100

    create widget

--- Forms/Sources/__OS/FormsApiX.cpp
+++ Forms/Sources/__OS/FormsApiX.cpp
@@ -25,7 +25,7 @@ namespace {
   static int32 exitCode = 0;
   static int32 defaultTextSize = 12;
 
-  class IWidget pcf_interface {
+  class FlWidget : public object {
   public:
     class WidgetEventArgs : public EventArgs {
     public:
@@ -53,55 +53,91 @@ namespace {
     EventHandler Paint;
     EventHandler Callback;
 
-    virtual void Draw() = 0;    
+    template<typename T>
+    void Draw(T& control) {
+      control.DrawControl();
+      this->Paint(object(), System::EventArgs());
+    }
+
+    template<typename T>
+    int HandleEvent(int event, T& control) {
+      WidgetEventArgs eventArgs(event, control.HandleControl(event) != 0);
+      this->EventHandled(object(), eventArgs);
+      return as<int32>(eventArgs.IsHandled());
+    }
+
+    static void __Callback__(Fl_Widget* widget, void* param) {
+      static_cast<FlWidget*>(param)->Callback(object(), System::EventArgs());
+    }
+
     virtual const Fl_Widget& ToWidget() const = 0;
     virtual Fl_Widget& ToWidget() = 0;
   };
 
-  template <typename T>
-  class Widget : public IWidget, public T {
+  class FlButton : public FlWidget, public Fl_Button {
   public:
-    Widget() : T(0, 0, 0, 0, "") {this->callback(&__Callback__, this);}
+    FlButton(int32 x, int32 y, int32 w, int32 h, const char* t) : Fl_Button(x, y, w, h, t) {}
+    void draw() override {this->Draw(*this);}
+    int handle(int event) override {return this->HandleEvent(event, *this);}
+    void DrawControl() {this->Fl_Button::draw();}
+    int32 HandleControl(int32 event) {return this->Fl_Button::handle(event);}
+    const Fl_Widget& ToWidget() const override {return *this;}
+    Fl_Widget& ToWidget() override {return *this;}
+  };
 
-    Widget(int width, int height) : T(0, 0, width, height, "") { this->callback(&__Callback__, this); }
+  class FlCheckBox : public FlWidget, public Fl_Check_Button {
+  public:
+    FlCheckBox(int32 x, int32 y, int32 w, int32 h, const char* t) : Fl_Check_Button(x, y, w, h, t) {}
+    void draw() override {this->Draw(*this);}
+    int handle(int event) override {return this->HandleEvent(event, *this);}
+    void DrawControl() {this->Fl_Check_Button::draw();}
+    int32 HandleControl(int32 event) {return this->Fl_Check_Button::handle(event);}
+    const Fl_Widget& ToWidget() const override {return *this;}
+    Fl_Widget& ToWidget() override {return *this;}
+  };
 
-    Widget(int width, int height, const char* text) : T(0, 0, width, height, text) { this->callback(&__Callback__, this); }
+  class FlControl : public FlWidget, public Fl_Widget {
+  public:
+    FlControl(int32 x, int32 y, int32 w, int32 h, const char* t) : Fl_Widget(x, y, w, h, t) {}
+    void draw() override {this->Draw(*this);}
+    int handle(int event) override {return this->HandleEvent(event, *this);}
+    void DrawControl() {this->Fl_Widget::draw();}
+    int32 HandleControl(int32 event) {return this->Fl_Widget::handle(event);}
+    const Fl_Widget& ToWidget() const override {return *this;}
+    Fl_Widget& ToWidget() override {return *this;}
+  };
 
-    Widget(int x, int y, int width, int height) : T(x, y, width, height, "") { this->callback(&__Callback__, this); }
+  class FlForm : public FlWidget, public Fl_Double_Window {
+  public:
+    FlForm(int32 x, int32 y, int32 w, int32 h, const char* t) : Fl_Double_Window(x, y, w, h, t) {}
+    void draw() override {this->Draw(*this);}
+    int handle(int event) override {return this->HandleEvent(event, *this);}
+    void DrawControl() {this->Fl_Double_Window::draw();}
+    int32 HandleControl(int32 event) {return this->Fl_Double_Window::handle(event);}
+    const Fl_Widget& ToWidget() const override {return *this;}
+    Fl_Widget& ToWidget() override {return *this;}
+  };
 
-    Widget(int x, int y, int width, int height, const char* text) : T(x, y, width, height, text) { this->callback(&__Callback__, this); }
+  class FlLabel : public FlWidget, public Fl_Box {
+  public:
+    FlLabel(int32 x, int32 y, int32 w, int32 h, const char* t) : Fl_Box(x, y, w, h, t) {}
+    void draw() override {this->Draw(*this);}
+    int handle(int event) override {return this->HandleEvent(event, *this);}
+    void DrawControl() {this->Fl_Box::draw();}
+    int32 HandleControl(int32 event) {return this->Fl_Box::handle(event);}
+    const Fl_Widget& ToWidget() const override {return *this;}
+    Fl_Widget& ToWidget() override {return *this;}
+  };
 
-    Widget(const char* text) : T(0, 0, 0, 0, text) { this->callback(&__Callback__, this); }
-
-    void Draw() override {
-      this->T::draw();
-    }
-
-    const Fl_Widget& ToWidget() const override {return as<T>(*this);}
-    Fl_Widget& ToWidget() override {return as<T>(*this);}
-
-    static Fl_Text_Buffer& ToText(void* text) {return *reinterpret_cast<Fl_Text_Buffer*>(text);}
-
-    static const Fl_Text_Buffer& ToText(const void* text) {return *reinterpret_cast<const Fl_Text_Buffer*>(text);}
-
-    void FreePosition(bool freePostion) {this->force_position(freePostion);}
-
-  private:
-    /*
-    void draw() override {
-      this->Paint(object(), System::EventArgs());
-    }
-
-    int handle(int event) override {
-      WidgetEventArgs eventArgs(event, T::handle(event) != 0);
-      this->EventHandled(object(), eventArgs);
-      return as<int32>(eventArgs.IsHandled());
-    }
-    */
-
-    static void __Callback__(Fl_Widget* widget, void* param) {
-      static_cast<Widget<T>*>(param)->Callback(object(), System::EventArgs());
-    }
+  class FlRadioButton : public FlWidget, public Fl_Round_Button {
+  public:
+    FlRadioButton(int32 x, int32 y, int32 w, int32 h, const char* t) : Fl_Round_Button(x, y, w, h, t) {}
+    void draw() override {this->Draw(*this);}
+    int handle(int event) override {return this->HandleEvent(event, *this);}
+    void DrawControl() {this->Fl_Round_Button::draw();}
+    int32 HandleControl(int32 event) {return this->Fl_Round_Button::handle(event);}
+    const Fl_Widget& ToWidget() const override {return *this;}
+    Fl_Widget& ToWidget() override {return *this;}
   };
 
   static Fl_Color FromColor(const System::Drawing::Color& color) {
@@ -146,21 +182,21 @@ void FormsApi::Control::Close(const System::Windows::Forms::Form& form) {
 }
 
 intptr FormsApi::Control::Create(const System::Windows::Forms::Button& button) {
-  Widget<Fl_Button>* handle = new Widget<Fl_Button>(button.Location().X, button.Location().Y, button.Size().Width, button.Size().Height, button.data().text.c_str());
+  FlButton* handle = new FlButton(button.Location().X, button.Location().Y, button.Size().Width, button.Size().Height, button.data().text.c_str());
   handle->color(FromColor(button.BackColor()));
   handle->labelcolor(FromColor(button.ForeColor()));
   handle->labelsize(defaultTextSize);
   handle->align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
-  return reinterpret_cast<intptr>(handle);
+  return (intptr)handle;
 }
 
 intptr FormsApi::Control::Create(const System::Windows::Forms::CheckBox& checkBox) {
-  Widget<Fl_Check_Button>* handle = new Widget<Fl_Check_Button>(checkBox.Location().X, checkBox.Location().Y, checkBox.Size().Width, checkBox.Size().Height, checkBox.data().text.c_str());
+  FlCheckBox* handle = new FlCheckBox(checkBox.Location().X, checkBox.Location().Y, checkBox.Size().Width, checkBox.Size().Height, checkBox.data().text.c_str());
   handle->color(FromColor(checkBox.BackColor()));
   handle->labelcolor(FromColor(checkBox.ForeColor()));
   handle->labelsize(defaultTextSize);
   handle->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
-  return reinterpret_cast<intptr>(handle);
+  return (intptr)handle;
 }
 
 intptr FormsApi::Control::Create(const System::Windows::Forms::Control& control) {
@@ -168,32 +204,32 @@ intptr FormsApi::Control::Create(const System::Windows::Forms::Control& control)
 }
 
 intptr FormsApi::Control::Create(const System::Windows::Forms::Form& form) {
-  IWidget* handle = new Widget<Fl_Double_Window>(form.Location().X, form.Location().Y, form.Size().Width, form.Size().Height, form.data().text.c_str());
-  handle->ToWidget().color(FromColor(form.BackColor()));
-  handle->ToWidget().labelcolor(FromColor(form.ForeColor()));
-  handle->ToWidget().labelsize(defaultTextSize);
-  ((Fl_Double_Window&)handle->ToWidget()).resizable(handle->ToWidget());
-  handle->ToWidget().align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
-  return reinterpret_cast<intptr>(handle);
+  FlForm* handle = new FlForm(form.Location().X, form.Location().Y, form.Size().Width, form.Size().Height, form.data().text.c_str());
+  handle->color(FromColor(form.BackColor()));
+  handle->labelcolor(FromColor(form.ForeColor()));
+  handle->labelsize(defaultTextSize);
+  handle->resizable(handle);
+  handle->align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+  return (intptr)handle;
 }
 
 intptr FormsApi::Control::Create(const System::Windows::Forms::Label& label) {
-  IWidget* handle = new Widget<Fl_Box>(label.Location().X, label.Location().Y, label.Size().Width, label.Size().Height, label.data().text.c_str());
-  handle->ToWidget().box(FL_NO_BOX);
-  handle->ToWidget().color(FromColor(label.BackColor()));
-  handle->ToWidget().labelcolor(FromColor(label.ForeColor()));
-  handle->ToWidget().labelsize(defaultTextSize);
-  handle->ToWidget().align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
-  return reinterpret_cast<intptr>(handle);
+  FlLabel* handle = new FlLabel(label.Location().X, label.Location().Y, label.Size().Width, label.Size().Height, label.data().text.c_str());
+  handle->box(FL_NO_BOX);
+  handle->color(FromColor(label.BackColor()));
+  handle->labelcolor(FromColor(label.ForeColor()));
+  handle->labelsize(defaultTextSize);
+  handle->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+  return (intptr)handle;
 }
 
 intptr FormsApi::Control::Create(const System::Windows::Forms::RadioButton& radioButton) {
-  Widget<Fl_Round_Button>* handle = new Widget<Fl_Round_Button>(radioButton.Location().X, radioButton.Location().Y, radioButton.Size().Width, radioButton.Size().Height, radioButton.data().text.c_str());
+  FlRadioButton* handle = new FlRadioButton(radioButton.Location().X, radioButton.Location().Y, radioButton.Size().Width, radioButton.Size().Height, radioButton.data().text.c_str());
   handle->color(FromColor(radioButton.BackColor()));
   handle->labelcolor(FromColor(radioButton.ForeColor()));
   handle->labelsize(defaultTextSize);
   handle->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
-  return reinterpret_cast<intptr>(handle);
+  return (intptr)handle;
 }
 
 void FormsApi::Control::DefWndProc(System::Windows::Forms::Message& message) {
@@ -201,11 +237,11 @@ void FormsApi::Control::DefWndProc(System::Windows::Forms::Message& message) {
 
 void FormsApi::Control::Destroy(const System::Windows::Forms::Control& control) {
   if (control.data().handle != IntPtr::Zero) {
-    //if (is<System::Windows::Forms::ContainerControl>(control)) {
-    //  for (int index = 0; index < ((Fl_Group&)(reinterpret_cast<IWidget*>(control.data().handle))->ToWidget()).children(); index++)
-    //    ((Fl_Group&)(reinterpret_cast<IWidget*>(control.data().handle))->ToWidget()).remove(index);
-    //}
-    delete reinterpret_cast<IWidget*>(control.data().handle);
+    if (is<System::Windows::Forms::ContainerControl>(control)) {
+      for (int index = 0; index < ((Fl_Group&)((FlWidget*)control.data().handle)->ToWidget()).children(); index++)
+        ((Fl_Group&)((FlWidget*)control.data().handle)->ToWidget()).remove(index);
+    }
+    delete (FlWidget*)control.data().handle;
   }
 }
 
@@ -228,21 +264,21 @@ System::Drawing::Point FormsApi::Control::PointToScreen(const System::Windows::F
 }
 
 void FormsApi::Control::SetBackColor(intptr hdc, const System::Drawing::Color& color) {
-  ((IWidget*)hdc)->ToWidget().color(FromColor(color));
+  ((FlWidget*)hdc)->ToWidget().color(FromColor(color));
 }
 
 void FormsApi::Control::SetForeColor(intptr hdc, const System::Drawing::Color& color) {
-  ((IWidget*)hdc)->ToWidget().labelcolor(FromColor(color));
+  ((FlWidget*)hdc)->ToWidget().labelcolor(FromColor(color));
 }
 
 void FormsApi::Control::SetBackColor(const System::Windows::Forms::Control& control) {
   if (control.data().handle != IntPtr::Zero)
-    (reinterpret_cast<IWidget*>(control.data().handle))->ToWidget().color(FromColor(control.BackColor()));
+    ((FlWidget*)control.data().handle)->ToWidget().color(FromColor(control.BackColor()));
 }
 
 void FormsApi::Control::SetForeColor(const System::Windows::Forms::Control& control) {
   if (control.data().handle != IntPtr::Zero)
-    (reinterpret_cast<IWidget*>(control.data().handle))->ToWidget().labelcolor(FromColor(control.ForeColor()));
+    ((FlWidget*)control.data().handle)->ToWidget().labelcolor(FromColor(control.ForeColor()));
 }
 
 void FormsApi::Control::SetLocation(const System::Windows::Forms::Control& control) {
@@ -253,11 +289,11 @@ void FormsApi::Control::SetSize(const System::Windows::Forms::Control& control) 
 
 void FormsApi::Control::SetText(const System::Windows::Forms::Control& control) {
   if (control.data().handle != IntPtr::Zero)
-    (reinterpret_cast<IWidget*>(control.data().handle))->ToWidget().copy_label(control.Text().c_str());
+    ((FlWidget*)control.data().handle)->ToWidget().copy_label(control.Text().c_str());
 }
 
 void FormsApi::Control::SetVisible(const System::Windows::Forms::Control& control) {
-  (reinterpret_cast<IWidget*>(control.data().handle))->ToWidget().show();
+  ((FlWidget*)control.data().handle)->ToWidget().show();
 }
 
 int32 FormsApi::SystemInformation::GetActiveWindowTrackingDelay() {
