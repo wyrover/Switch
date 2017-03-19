4634de0c64bedb16fdcebbfe51958fb08e3d5215
yfiumefreddo <y.fiumefreddo@gmail.com>
2017-03-19 23:37:23 +0100

    Forms implementation

--- Forms/Sources/Pcf/System/Windows/Forms/Message.cpp
+++ Forms/Sources/Pcf/System/Windows/Forms/Message.cpp
@@ -8,5 +8,5 @@ using namespace System::Windows::Forms;
 int32 System::Windows::Forms::Message::counter = 0;
 
 string Message::ToString() const noexcept {
-  return string::Format("counter={6} msg=0x{0:X} ({1}) hwnd=0x{2:X} wparam=0x{3:X} lparam=0x{4:X} result=0x{5:X}", this->msg, __OS::FormsApi::Control::MessageToString(this->msg), this->hwnd, this->WParam, this->lParam, this->result, counter);
+  return string::Format("msg=0x{0:X} ({1}) hwnd=0x{2:X} wparam=0x{3:X} lparam=0x{4:X} result=0x{5:X}", this->msg, __OS::FormsApi::Control::MessageToString(this->msg), this->hwnd, this->WParam, this->lParam, this->result);
 }
