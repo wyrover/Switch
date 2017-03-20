ed9741ce93bd017f35e5295d7548472f0e9f9e3b
yfiumefreddo <y.fiumefreddo@gmail.com>
2017-03-20 16:28:04 +0100

    Forms implementation on Linux

--- Forms/Sources/__OS/FormsApiX.cpp
+++ Forms/Sources/__OS/FormsApiX.cpp
@@ -13,6 +13,7 @@
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Check_Button.H>
+#include <FL/fl_draw.H>
 #include <FL/Fl_Double_Window.H>
 #include <FL/Fl_File_Icon.H>
 #include <FL/Fl_Pixmap.H>
@@ -443,6 +444,16 @@ void FormsApi::Application::Exit() {
 void FormsApi::Application::MessageLoop(EventHandler idle) {
   exitCode = Fl::run();
 }
+
+void FormsApi::Application::MessageBeep(MessageBoxIcon type) {
+  System::Collections::Generic::SortedDictionary<MessageBoxIcon, int32> beep = {{(MessageBoxIcon)0, FL_BEEP_DEFAULT}, {MessageBoxIcon::Error, FL_BEEP_ERROR}, {MessageBoxIcon::Question, FL_BEEP_QUESTION}, {MessageBoxIcon::Warning, FL_BEEP_MESSAGE}, {MessageBoxIcon::Information, FL_BEEP_NOTIFICATION}, {(MessageBoxIcon)0xFFFFFFFF, FL_BEEP_DEFAULT}};
+  fl_beep(beep[type]);
+}
+
+void MyMessageBeep(MessageBoxIcon type) {
+  FormsApi::Application::MessageBeep(type);
+}
+
 namespace {
   DialogResult ShowMessageBoxAbortRetryIgnore(const string& message) {
     int result = fl_choice("%s", "Abort", "Retry", "Ignore", message.c_str());
@@ -509,9 +520,21 @@ namespace {
   static int ApplicationHandler(int event) {
     return event == FL_SHORTCUT && Fl::event_key() == FL_Escape ? 1 : 0;
   }
+
+  static void DrawLabel(const Fl_Label *label, int left, int top, int width, int height, Fl_Align align) {
+    fl_font(label->font, label->size);
+    fl_color((Fl_Color)label->color);
+    fl_draw(label->value, left, top, width, height, align, label->image, 0 /*1 (default): replace patterns (like "@->") by symbols; 0: do not replace patterns by symbols*/);
+  }
+
+  static void MeasureLabel(const Fl_Label *label, int &width, int &height) {
+    fl_font(label->font, label->size);
+    fl_measure(label->value, width, height, 0 /*1 (default): replace patterns (like "@->") by symbols; 0: do not replace patterns by symbols*/);
+  }
 }
 
 void FormsApi::Application::Start() {
+  Fl::set_labeltype(FL_NORMAL_LABEL, DrawLabel, MeasureLabel);
   Fl::get_system_colors();
   Fl_File_Icon::load_system_icons();
   Fl::lock();
@@ -636,238 +659,6 @@ void FormsApi::Control::SetText(const System::Windows::Forms::Control& control) 
 
 void FormsApi::Control::SetVisible(const System::Windows::Forms::Control& control) {
   ((FlWidget*)control.data().handle)->ToWidget().show();
-}
-
-int32 FormsApi::SystemInformation::GetActiveWindowTrackingDelay() {
-  return 0;
-}
-
-ArrangeDirection FormsApi::SystemInformation::GetArrangeDirection() {
-  return ArrangeDirection::Left;
-}
-
-ArrangeStartingPosition FormsApi::SystemInformation::GetArrangeStartingPosition() {
-  return ArrangeStartingPosition::Hide;
-}
-
-BootMode FormsApi::SystemInformation::GetBootMode() {
-  return BootMode::Normal;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetBorder3DSize() {
-  return System::Drawing::Size(1, 1);
-}
-
-int32 FormsApi::SystemInformation::GetBorderMultiplierFactor() {
-  return 1;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetBorderSize() {
-  return System::Drawing::Size(1, 1);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetCaptionButtonSize() {
-  return System::Drawing::Size(16, 16);
-}
-
-int32 FormsApi::SystemInformation::GetCaptionHeight() {
-  return 23;
-}
-
-int32 FormsApi::SystemInformation::GetCaretBlinkTime() {
-  return 530;
-}
-
-int32 FormsApi::SystemInformation::GetCaretWidth() {
-  return 1;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetCursorSize() {
-  return System::Drawing::Size(32, 32);
-}
-
-bool FormsApi::SystemInformation::GetDbcsEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetDebugOS() {
-  return false;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetDoubleClickSize() {
-  return System::Drawing::Size(4, 4);
-}
-
-int32 FormsApi::SystemInformation::GetDoubleClickTime() {
-  return 500;
-}
-
-bool FormsApi::SystemInformation::GetDragFullWindows() {
-  return true;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetDragSize() {
-  return System::Drawing::Size(4, 4);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetFixedFrameBorderSize() {
-  return System::Drawing::Size(0, 0);
-}
-
-int32 FormsApi::SystemInformation::GetFontSmoothingContrast() {
-  return 1200;
-}
-
-int32 FormsApi::SystemInformation::GetFontSmoothingType() {
-  return 2;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetFrameBorderSize() {
-  return System::Drawing::Size(0, 0);
-}
-
-bool FormsApi::SystemInformation::GetHighContrast() {
-  return false;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalFocusThickness() {
-  return 1;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalResizeBorderThickness() {
-  return 4;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalScrollBarArrowWidth() {
-  return 17;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalScrollBarHeight() {
-  return 17;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalScrollBarThumbWidth() {
-  return 17;
-}
-
-int32 FormsApi::SystemInformation::GetIconHorizontalSpacing() {
-  return 75;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetIconSize() {
-  return System::Drawing::Size(64, 64);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetIconSpacingSize() {
-  return System::Drawing::Size(75, 75);
-}
-
-int32 FormsApi::SystemInformation::GetIconVerticalSpacing() {
-  return 75;
-}
-
-bool FormsApi::SystemInformation::GetIsActiveWindowTrackingEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsComboBoxAnimationEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsDropShadowEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsFlatMenuEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsFontSmoothingEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsHotTrackingEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsIconTitleWrappingEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsKeyboardPreferred() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsListBoxSmoothScrollingEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsMenuAnimationEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsMenuFadeEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsMinimizeRestoreAnimationEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsSelectionFadeEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsSnapToDefaultEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsTitleBarGradientEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsToolTipAnimationEnabled() {
-  return false;
-}
-
-int32 FormsApi::SystemInformation::GetKanjiWindowHeight() {
-  return 0;
-}
-
-int32 FormsApi::SystemInformation::GetKeyboardDelay() {
-  return 1;
-}
-
-int32 FormsApi::SystemInformation::GetKeyboardSpeed() {
-  return 31;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetMaxWindowTrackSize() {
-  return System::Drawing::Size(0, 0);
-}
-
-bool FormsApi::SystemInformation::GetMenuAccessKeysUnderlined() {
-  return false;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetMenuBarButtonSize() {
-  return System::Drawing::Size(19, 19);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetMenuButtonSize() {
-  return System::Drawing::Size(19, 19);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetMenuCheckSize() {
-  return System::Drawing::Size(15, 15);
-}
-
-System::Drawing::Font FormsApi::SystemInformation::GetMenuFont() {
-  return System::Drawing::Font::FromLogFont((intptr)0);
-}
-
-int32 FormsApi::SystemInformation::GetMenuHeight() {
-  return 23;
 }
 
 #endif
