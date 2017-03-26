846dc5667a3cb06aaeacbc03c819554e735a161a
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-03-26 21:24:58 +0200

    Forms implementation on Mac os X

--- Forms/Sources/__OS/FormsApiCocoa.mm
+++ Forms/Sources/__OS/FormsApiCocoa.mm
@@ -33,6 +33,26 @@ namespace {
       pcf_set {this->controls = value;}
     };
     
+    NSWindowStyleMask FormToNSWindowStyleMask(const Form& form) {
+      constexpr int CP_NOCLOSE_BUTTON = 0x200;
+      NSWindowStyleMask windowStyleMask = NSWindowStyleMaskBorderless;
+      
+      if (form.FormBorderStyle != FormBorderStyle::None) {
+        windowStyleMask |= NSWindowStyleMaskTitled;
+        
+        if (form.MaximizeBox && (form.FormBorderStyle == FormBorderStyle::Sizable || form.FormBorderStyle == FormBorderStyle::SizableToolWindow))
+          windowStyleMask |= NSWindowStyleMaskResizable;
+        
+        if (form.MinimizeBox)
+          windowStyleMask |= NSWindowStyleMaskMiniaturizable;
+        
+        if (CP_NOCLOSE_BUTTON /*(form.GetCreateParams().ClassStyle | CP_NOCLOSE_BUTTON) == CP_NOCLOSE_BUTTON*/)
+          windowStyleMask |= NSWindowStyleMaskClosable;
+      }
+      
+      return windowStyleMask;
+    }
+    
     System::Drawing::Rectangle GetBounds(const System::Windows::Forms::Control& control) {
       if (is<System::Windows::Forms::Form>(control))
         return this->GetBounds(as<System::Windows::Forms::Form>(control));
@@ -170,17 +190,23 @@ namespace {
     }
     
     void DispatchMessage(NSEvent* event) {
-      static System::Collections::Generic::SortedDictionary<int32, delegate<void, NSEvent*, Control&>> events = {
-        {NSEventTypeMouseEntered, {*this, &CocoaApi::MouseEnterEvent}},
-        {NSEventTypeMouseExited, {*this, &CocoaApi::MouseLeaveEvent}},
-        {NSEventTypeLeftMouseDown, {*this, &CocoaApi::LeftMouseDownEvent}},
-        {NSEventTypeLeftMouseUp, {*this, &CocoaApi::LeftMouseUpEvent}},
-        {NSEventTypeRightMouseDown, {*this, &CocoaApi::RightMouseDownEvent}},
-        {NSEventTypeRightMouseUp, {*this, &CocoaApi::RightMouseUpEvent}},
-        {NSEventTypeMouseMoved, {*this, &CocoaApi::MouseMoveEvent}},
-        {NSEventTypeOtherMouseDown, {*this, &CocoaApi::OtherMouseDownEvent}},
-        {NSEventTypeOtherMouseUp, {*this, &CocoaApi::OtherMouseUpEvent}},
-      };
+      /*
+       //{NSEventTypeLeftMouseDragged, WM_...},
+       //{NSEventTypeRightMouseDragged, WM_...},
+       {NSEventTypeKeyDown, WM_KEYDOWN},
+       {NSEventTypeKeyUp, WM_KEYUP},
+       //{NSEventTypeFlagsChanged, WM_...},
+       //{NSEventTypeAppKitDefined, WM_...},
+       //{NSEventTypeSystemDefined, WM_...},
+       //{NSEventTypeApplicationDefined, WM_...},
+       //{NSEventTypePeriodic, WM_...},
+       {NSEventTypeCursorUpdate, WM_SETCURSOR},
+       {NSEventTypeScrollWheel, WM_MOUSEWHEEL},
+       //{NSEventTypeTabletPoint, WM_...},
+       //{NSEventTypeTabletProximity, WM_...},
+       //{NSEventTypeOtherMouseDragged, WM_...},
+       */
+      static System::Collections::Generic::SortedDictionary<int32, delegate<void, NSEvent*, Control&>> events = {{NSEventTypeMouseEntered, {*this, &CocoaApi::MouseEnterEvent}}, {NSEventTypeMouseExited, {*this, &CocoaApi::MouseLeaveEvent}}, {NSEventTypeLeftMouseDown, {*this, &CocoaApi::LeftMouseDownEvent}}, {NSEventTypeLeftMouseUp, {*this, &CocoaApi::LeftMouseUpEvent}}, {NSEventTypeRightMouseDown, {*this, &CocoaApi::RightMouseDownEvent}}, {NSEventTypeRightMouseUp, {*this, &CocoaApi::RightMouseUpEvent}}, {NSEventTypeMouseMoved, {*this, &CocoaApi::MouseMoveEvent}}, {NSEventTypeOtherMouseDown, {*this, &CocoaApi::OtherMouseDownEvent}}, {NSEventTypeOtherMouseUp, {*this, &CocoaApi::OtherMouseUpEvent}}};
       @autoreleasepool {
         if (events.ContainsKey([event type]) && controls.ContainsKey((intptr)[event window])) {
           events[[event type]](event, this->controls[(intptr)[event window]]);
@@ -261,48 +287,6 @@ namespace {
   };
   
   UniquePointer<CocoaApi> cocoaApi;
-  
-  NSWindowStyleMask FormToNSWindowStyleMask(const Form& form) {
-    constexpr int CP_NOCLOSE_BUTTON = 0x200;
-    NSWindowStyleMask windowStyleMask = NSWindowStyleMaskBorderless;
-    
-    if (form.FormBorderStyle != FormBorderStyle::None) {
-      windowStyleMask |= NSWindowStyleMaskTitled;
-      
-      if (form.MaximizeBox && (form.FormBorderStyle == FormBorderStyle::Sizable || form.FormBorderStyle == FormBorderStyle::SizableToolWindow))
-      windowStyleMask |= NSWindowStyleMaskResizable;
-      
-      if (form.MinimizeBox)
-      windowStyleMask |= NSWindowStyleMaskMiniaturizable;
-      
-      if (CP_NOCLOSE_BUTTON /*(form.GetCreateParams().ClassStyle | CP_NOCLOSE_BUTTON) == CP_NOCLOSE_BUTTON*/)
-      windowStyleMask |= NSWindowStyleMaskClosable;
-    }
-    
-    return windowStyleMask;
-  }
-  
-  Message NSEventToMessage(NSEvent* event) {
-    @autoreleasepool {
-      static System::Collections::Generic::Dictionary<int32, int32> events {
-        //{NSEventTypeLeftMouseDragged, WM_...},
-        //{NSEventTypeRightMouseDragged, WM_...},
-        {NSEventTypeKeyDown, WM_KEYDOWN},
-        {NSEventTypeKeyUp, WM_KEYUP},
-        //{NSEventTypeFlagsChanged, WM_...},
-        //{NSEventTypeAppKitDefined, WM_...},
-        //{NSEventTypeSystemDefined, WM_...},
-        //{NSEventTypeApplicationDefined, WM_...},
-        //{NSEventTypePeriodic, WM_...},
-        {NSEventTypeCursorUpdate, WM_SETCURSOR},
-        {NSEventTypeScrollWheel, WM_MOUSEWHEEL},
-        //{NSEventTypeTabletPoint, WM_...},
-        //{NSEventTypeTabletProximity, WM_...},
-        //{NSEventTypeOtherMouseDragged, WM_...},
-      };
-      return Message();
-    }
-  }
 }
 
 bool FormsApi::Application::visualStylesEnabled = false;
@@ -443,8 +427,9 @@ void FormsApi::Control::Close(const System::Windows::Forms::Form& form) {
 
 @implementation NSControlResponder
 - (IBAction) ControlClick:(id)sender {
-  //Message event = Message::Create((intptr)sender, WM_LBUTTONUP, 0, mouseDownLocation.X() + (mouseDownLocation.Y() << 16), 0, 0);
-  //const_cast<Control&>(cocoaApi().Controls()[(intptr)sender]()).WndProc(event);
+  System::Drawing::Point mouseDownLocation;
+  Message event = Message::Create((intptr)sender, WM_LBUTTONUP, 0, mouseDownLocation.X() + (mouseDownLocation.Y() << 16), 0, 0);
+  const_cast<Control&>(cocoaApi().Controls()[(intptr)sender]()).WndProc(event);
 }
 @end
 
@@ -458,8 +443,8 @@ intptr FormsApi::Control::Create(const System::Windows::Forms::Button& button) {
     [handle setButtonType:NSMomentaryPushInButton];
     [handle setBezelStyle:bounds.Height == button.DefaultSize().Height ? NSBezelStyleRounded : NSBezelStyleRegularSquare];
     [handle setAutoresizingMask:NSViewMaxXMargin | NSViewMinYMargin];
-    //[handle setTarget:[NSControlResponder alloc]];
-    //[handle setAction:@selector(ControlClick:)];
+    [handle setTarget:[NSControlResponder alloc]];
+    [handle setAction:@selector(ControlClick:)];
     cocoaApi().Controls()[(intptr)handle] = button;
     Message message = Message::Create((intptr)handle, WM_CREATE, 0, 0, 0, IntPtr::Zero);
     const_cast<System::Windows::Forms::Button&>(button).WndProc(message);
@@ -506,7 +491,7 @@ intptr FormsApi::Control::Create(const System::Windows::Forms::Form& form) {
     System::Drawing::Rectangle bounds = cocoaApi().GetBounds(form);
     NSWindow* handle = [[NSWindow alloc] init];
     
-    [handle setStyleMask: FormToNSWindowStyleMask(form)];
+    [handle setStyleMask: cocoaApi().FormToNSWindowStyleMask(form)];
     [handle setFrame:NSMakeRect(0, 0, bounds.Width(), bounds.Height()) display:YES];
     [handle setFrameTopLeftPoint:NSMakePoint(bounds.X(), bounds.Y())];
     
@@ -660,238 +645,6 @@ void FormsApi::Control::SetVisible(const System::Windows::Forms::Control& contro
       [(NSControl*)control.data->handle setHidden:control.data->visible ? NO : YES];
     }
   }
-}
-
-int32 FormsApi::SystemInformation::GetActiveWindowTrackingDelay() {
-  return 0;
-}
-
-ArrangeDirection FormsApi::SystemInformation::GetArrangeDirection() {
-  return ArrangeDirection::Left;
-}
-
-ArrangeStartingPosition FormsApi::SystemInformation::GetArrangeStartingPosition() {
-  return ArrangeStartingPosition::Hide;
-}
-
-BootMode FormsApi::SystemInformation::GetBootMode() {
-  return BootMode::Normal;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetBorder3DSize() {
-  return System::Drawing::Size(0, 0);
-}
-
-int32 FormsApi::SystemInformation::GetBorderMultiplierFactor() {
-  return 1;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetBorderSize() {
-  return System::Drawing::Size(0, 0);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetCaptionButtonSize() {
-  return System::Drawing::Size(16, 16);
-}
-
-int32 FormsApi::SystemInformation::GetCaptionHeight() {
-  return 23;
-}
-
-int32 FormsApi::SystemInformation::GetCaretBlinkTime() {
-  return 530;
-}
-
-int32 FormsApi::SystemInformation::GetCaretWidth() {
-  return 1;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetCursorSize() {
-  return System::Drawing::Size(32, 32);
-}
-
-bool FormsApi::SystemInformation::GetDbcsEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetDebugOS() {
-  return false;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetDoubleClickSize() {
-  return System::Drawing::Size(4, 4);
-}
-
-int32 FormsApi::SystemInformation::GetDoubleClickTime() {
-  return 500;
-}
-
-bool FormsApi::SystemInformation::GetDragFullWindows() {
-  return true;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetDragSize() {
-  return System::Drawing::Size(4, 4);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetFixedFrameBorderSize() {
-  return System::Drawing::Size(0, 0);
-}
-
-int32 FormsApi::SystemInformation::GetFontSmoothingContrast() {
-  return 1200;
-}
-
-int32 FormsApi::SystemInformation::GetFontSmoothingType() {
-  return 2;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetFrameBorderSize() {
-  return System::Drawing::Size(0, 0);
-}
-
-bool FormsApi::SystemInformation::GetHighContrast() {
-  return false;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalFocusThickness() {
-  return 1;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalResizeBorderThickness() {
-  return 4;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalScrollBarArrowWidth() {
-  return 17;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalScrollBarHeight() {
-  return 17;
-}
-
-int32 FormsApi::SystemInformation::GetHorizontalScrollBarThumbWidth() {
-  return 17;
-}
-
-int32 FormsApi::SystemInformation::GetIconHorizontalSpacing() {
-  return 75;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetIconSize() {
-  return System::Drawing::Size(64, 64);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetIconSpacingSize() {
-  return System::Drawing::Size(75, 75);
-}
-
-int32 FormsApi::SystemInformation::GetIconVerticalSpacing() {
-  return 75;
-}
-
-bool FormsApi::SystemInformation::GetIsActiveWindowTrackingEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsComboBoxAnimationEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsDropShadowEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsFlatMenuEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsFontSmoothingEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsHotTrackingEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsIconTitleWrappingEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsKeyboardPreferred() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsListBoxSmoothScrollingEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsMenuAnimationEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsMenuFadeEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsMinimizeRestoreAnimationEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsSelectionFadeEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsSnapToDefaultEnabled() {
-  return false;
-}
-
-bool FormsApi::SystemInformation::GetIsTitleBarGradientEnabled() {
-  return true;
-}
-
-bool FormsApi::SystemInformation::GetIsToolTipAnimationEnabled() {
-  return false;
-}
-
-int32 FormsApi::SystemInformation::GetKanjiWindowHeight() {
-  return 0;
-}
-
-int32 FormsApi::SystemInformation::GetKeyboardDelay() {
-  return 1;
-}
-
-int32 FormsApi::SystemInformation::GetKeyboardSpeed() {
-  return 31;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetMaxWindowTrackSize() {
-  return System::Drawing::Size(0, 0);
-}
-
-bool FormsApi::SystemInformation::GetMenuAccessKeysUnderlined() {
-  return false;
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetMenuBarButtonSize() {
-  return System::Drawing::Size(19, 19);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetMenuButtonSize() {
-  return System::Drawing::Size(19, 19);
-}
-
-System::Drawing::Size FormsApi::SystemInformation::GetMenuCheckSize() {
-  return System::Drawing::Size(15, 15);
-}
-
-Font FormsApi::SystemInformation::GetMenuFont() {
-  return Font::FromLogFont((intptr)0);
-}
-
-int32 FormsApi::SystemInformation::GetMenuHeight() {
-  return 23;
 }
 
 #endif
