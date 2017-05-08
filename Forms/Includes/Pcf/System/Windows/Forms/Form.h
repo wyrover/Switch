18bae7cd615dab25af4c5aeb8d8276fc45bf56bf
yfiumefreddo <y.fiumefreddo@gmail.com>
2017-05-08 21:37:15 +0200

    Fix Form contructor

--- Forms/Includes/Pcf/System/Windows/Forms/Form.h
+++ Forms/Includes/Pcf/System/Windows/Forms/Form.h
@@ -20,7 +20,7 @@ namespace Pcf {
         /// @brief Represents a window or dialog box that makes up an application's user interface.
         class pcf_public Form : public ContainerControl {
         public:
-          Form() {
+          Form() : ContainerControl("", 0, 0, 300, 300) {
             this->visible = false;
             this->SetStyle(ControlStyles::UserPaint, false);
             //this->BackColor = System::Drawing::SystemColors::Window;
