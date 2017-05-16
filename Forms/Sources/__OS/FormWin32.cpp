d9a8118e320762c52c0f0c99f32a171765311dd2
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-05-16 22:32:43 +0200

    fix Location on Win32

--- Forms/Sources/__OS/FormWin32.cpp
+++ Forms/Sources/__OS/FormWin32.cpp
@@ -24,11 +24,15 @@ intptr FormsApi::Form::Create(System::Windows::Forms::Form& form) {
   case FormStartPosition::WindowsDefaultBounds: bounds = Drawing::Rectangle(CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT); break;
   case FormStartPosition::WindowsDefaultLocation: bounds = Drawing::Rectangle(CW_USEDEFAULT, CW_USEDEFAULT, form.Width, form.Height); break;
   }
-  form.Location= System::Drawing::Point(bounds.Left, bounds.Top);
-  form.Size= System::Drawing::Size(bounds.Width, bounds.Height);
 
   HWND handle = CreateWindowEx(0, WC_DIALOG, form.Text().w_str().c_str(), WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_GROUP, bounds.Left, bounds.Top, bounds.Width, bounds.Height, NULL, (HMENU)0, __instance, (LPVOID)NULL);
   WindowProcedure::DefWindowProcs[(intptr)handle] = (WNDPROC)SetWindowLongPtr(handle, GWLP_WNDPROC, (LONG_PTR)WindowProcedure::WndProc);
+
+  RECT rect = { 0, 0, 0, 0 };
+  GetWindowRect(handle, &rect);
+  form.Location = System::Drawing::Point(rect.left, rect.top);
+  form.Size = System::Drawing::Size(rect.right - rect.left, rect.bottom - rect.top);
+
   /// @todo to remove after create SetFont method...
   PostMessage(handle, WM_SETFONT, WPARAM((HFONT)GetStockObject(DEFAULT_GUI_FONT)), TRUE);
   return (intptr)handle;
