3fe0aee0b357ebf2a489a4bfc28260188938f14e
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-05-22 23:08:22 +0200

    Update install_ubuntu_gcc.sh

--- install_ubuntu_gcc.sh
+++ install_ubuntu_gcc.sh
@@ -13,6 +13,9 @@ mkdir -p Build/ThirdParties
 sudo apt update
 sudo apt install clang cmake doxygen libcurlpp-dev libfltk1.3-dev libgtkmm-3.0-dev libjpeg8-dev libpng-dev libssl-dev libx11-dev uuid-dev zlibc
 
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang
+
 cd Build/ThirdParties
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../../ThirdParties 
 make install -j6
