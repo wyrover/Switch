83b6f171d1b86f3ef7bc0e5f707a8592c77e7a86
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-05-25 00:28:50 +0200

    install on linux

--- Core/ManualTests/Main.cpp
+++ Core/ManualTests/Main.cpp
@@ -1,56 +1,12 @@
 #include <Pcf/Core.h>
 
 using namespace System;
-using namespace System::IO;
 
 namespace Examples {
   class Program {
   public:
     static void Main() {
-      //Console::WriteLine("Hello, World!");
-      Console::WriteLine("{0}", DateTime(1601, 1, 5));
-      
-      delegate<void, const string&> dlgA = pcf_delegate(const string& s) {
-        Console::WriteLine("LA = {0}", s);
-      };
-      
-      delegate<void, const string&> dlgB = pcf_delegate(const string& s) {
-        Console::WriteLine("LB = {0}", s);
-      };
-      
-      delegate<void, const string&> dlgC = pcf_delegate(const string& s) {
-        Console::WriteLine("LC = {0}", s);
-      };
-      
-      class Fct {
-      public:
-        Fct(const string& id): id(id) {}
-        void operator()(const string& s) {
-          Console::WriteLine("{0} = {0}", id, s);
-        }
-        
-      private:
-        string id;
-      };
-      
-      delegate<void, const string&> all;
-      all += dlgA;
-      all += dlgB;
-      all += dlgC;
-      
-      Fct fctA("FA");
-      Fct fctB("FB");
-      Fct fctC("FC");
-      
-      all += fctA;
-      all += fctB;
-      all += fctC;
-      
-      all -= dlgB;
-      all -= fctB;
-      
-      all("Test");
-      
+      Console::WriteLine("Hello, World!");
     }
   };
 }
--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ if [[ "$OSTYPE" != *"Darwin"* ]]; then
 fi
 
 case "$OSTYPE" in
+  *"CentOS"*) ./install_redhat_clang.sh;;
   *"Darwin"*) ./install_macosx_xcode.sh;;
-  *"Ubuntu"*) ./install_ubuntu_clang.sh;;
-  *"CentOS"*) ./install_centos_clang.sh;;
+  *"elementary"*) ./install_debian_clang.sh;;
+  *"Fedora"*) ./install_redhat_clang.sh;;
+  *"RedHat"*) ./install_redhat_clang.sh;;
+  *"Suze"*) ./install_suze_clang.sh;;
+  *"Ubuntu"*) ./install_debian_clang.sh;;
   *) ./install_linux_clang.sh;;
 esac
