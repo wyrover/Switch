811ba0fea20ef3c63c2bc10c0d1a09d9cfe15b22
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-05-25 12:11:09 +0200

    install on linux

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ fi
 case "$OSTYPE" in
   *"CentOS"*) ./install_centos_clang.sh;;
   *"Darwin"*) ./install_macosx_xcode.sh;;
-  *"elementary"*) ./install_debian_clang.sh;;
-  *"Fedora"*) ./install_redhat_clang.sh;;
+  *"Debian"*) ./install_debian_clang.sh;;
+  *"elementary"*) ./install_elementary_os_clang.sh;;
+  *"Fedora"*) ./install_fedora_clang.sh;;
+  *"Mint"*) ./install_mint_clang.sh;;
   *"RedHat"*) ./install_redhat_clang.sh;;
   *"Suze"*) ./install_suze_clang.sh;;
-  *"Ubuntu"*) ./install_debian_clang.sh;;
+  *"Ubuntu"*) ./install_ubuntu_clang.sh;;
   *) ./install_linux_clang.sh;;
 esac
--- install_mint_clang.sh
+++ install_mint_clang.sh
@@ -7,20 +7,11 @@ sudo chmod 777 /usr/local/bin
 sudo chmod 777 /usr/local/cmake
 sudo chmod 777 /usr/local/include
 sudo chmod 777 /usr/local/lib
-# --> To remove when fltk removed from project
-sudo chmod 777 /usr/local/share
-sudo chmod 777 /usr/local/share/man
-sudo chmod 777 /usr/local/share/man/man1
-sudo chmod 777 /usr/local/share/man/man3
-sudo chmod 777 /usr/local/share/man/man6
-sudo chmod 777 /usr/local/share/applications
-sudo chmod 777 /usr/local/lib64
-# <--
 mkdir -p Build/Examples
 mkdir -p Build/ThirdParties
 
-sudo yum update
-sudo yum install clang cmake doxygen libcurl-devel libjpeg-devel zlib-devel libpng-devel libX11-devel libuuid-devel uuid-devel gtkmm30-devel
+sudo apt update
+sudo apt install clang cmake doxygen libcurlpp-dev libfltk1.3-dev libgtkmm-3.0-dev libjpeg8-dev libpng-dev libssl-dev libx11-dev uuid-dev zlibc
 
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
