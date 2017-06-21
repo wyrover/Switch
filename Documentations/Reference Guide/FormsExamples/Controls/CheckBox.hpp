c1eddc63d96e15b09e59ad66f0e81523c83deb7d
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-06-21 09:05:22 +0200

    image scaling

--- Documentations/Reference Guide/FormsExamples/Controls/CheckBox.hpp
+++ Documentations/Reference Guide/FormsExamples/Controls/CheckBox.hpp
@@ -1,5 +1,11 @@
-/**
- * @page FormslibCheckBoxPage CheckBox
- * The following code example shows how to use Pcf::System::Windows::Forms::CheckBox class.
- * @include CheckBox.cpp
- */
+/// @page FormslibCheckBoxPage CheckBox
+/// The following code example shows how to use Pcf::System::Windows::Forms::CheckBox control.
+/// @include CheckBox.cpp
+///
+/// @image HTML Images/Examples/Forms/MacOsX/CheckBox.png "Mac Os X"
+///
+/// @image HTML Examples/Forms/Ubuntu/CheckBox "Ubuntu"
+///
+/// @image HTML Examples/Forms/Windows/CheckBox "Windows"
+///
+
