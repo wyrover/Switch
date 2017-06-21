90aa2dba298d9f0ea314674e2c52af8d6754c259
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-06-21 11:32:49 +0200

    add documentations

--- Documentations/Reference Guide/FormsExamples/Application/Application.hpp
+++ Documentations/Reference Guide/FormsExamples/Application/Application.hpp
@@ -1,9 +1,10 @@
+/// @page FormslibApplicationPage Application
 /// The following code example shows how to use Pcf::System::Windows::Forms::Application class.
 /// @include Application.cpp
 /// <BR>
-/// @image HTML Images/Examples/Forms/ApplicationM.jpg "Mac Os X"
+/// @image HTML Images/Examples/Forms/ApplicationM.png "Mac Os X"
 /// <BR>
-/// @image HTML Images/Examples/Forms/ApplicationU.jpg "Ubuntu"
+/// @image HTML Images/Examples/Forms/ApplicationU.png "Ubuntu"
 /// <BR>
-/// @image HTML Images/Examples/Forms/ApplicationW.jpg "Windows"
+/// @image HTML Images/Examples/Forms/ApplicationW.png "Windows"
 /// <BR>
---
+++ Examples/Forms/Application/README.md
@@ -0,0 +1,15 @@
+# Application
+This example demonstrates the use of System::Windows::Forms::Application class.
+<BR>
+
+![GitHub Logo](../../../Documentations/Images/Examples/Forms/ApplicationM.png)
+<p align="left">Mac Os X</p>
+<BR>
+
+![GitHub Logo](../../../Documentations/Images/Examples/Forms/ApplicationU.png)
+<p align="left">Ubuntu</p>
+<BR>
+
+![GitHub Logo](../../../Documentations/Images/Examples/Forms/ApplicationW.png)
+<p align="left">Windows</p>
+<BR>
