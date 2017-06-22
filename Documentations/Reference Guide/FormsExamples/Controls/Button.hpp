b66dfa88f5e38ba7bb1910674cb7c89fe8f4713b
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-06-22 22:07:28 +0200

    add documentations

--- Documentations/Reference Guide/FormsExamples/Controls/Button.hpp
+++ Documentations/Reference Guide/FormsExamples/Controls/Button.hpp
@@ -1,5 +1,10 @@
-/**
- * @page FormslibButtonPage Button
- * The following code example shows how to use Pcf::System::Windows::Forms::Button class.
- * @include Button.cpp
- */
+/// @page FormslibButtonPage Button
+/// The following code example shows how to use Pcf::System::Windows::Forms::Button class.
+/// @include Button.cpp
+/// <BR>
+/// @image HTML Images/Examples/Forms/ButtonM.png "Mac Os X"
+/// <BR>
+/// @image HTML Images/Examples/Forms/ButtonU.png "Ubuntu"
+/// <BR>
+/// @image HTML Images/Examples/Forms/ButtonW.png "Windows"
+/// <BR>
--- Documentations/Reference Guide/FormsExamples/Controls/SystemSound.hpp
+++ Documentations/Reference Guide/FormsExamples/Controls/SystemSound.hpp
@@ -1,5 +1,10 @@
-/**
- * @page FormslibSystemSoundPage SystemSound
- * The following code example shows how to use Pcf::System::Media::SystemSound class.
- * @include SystemSound.cpp
- */
+/// @page FormslibSystemSoundPage SystemSound
+/// The following code example shows how to use Pcf::System::Media::SystemSound class.
+/// @include SystemSound.cpp
+/// <BR>
+/// @image HTML Images/Examples/Forms/SystemSoundM.png "Mac Os X"
+/// <BR>
+/// @image HTML Images/Examples/Forms/SystemSoundU.png "Ubuntu"
+/// <BR>
+/// @image HTML Images/Examples/Forms/SystemSoundW.png "Windows"
+/// <BR>
