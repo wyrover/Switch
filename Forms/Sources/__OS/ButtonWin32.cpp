3a3d441fa31a5dcd96bf9100584be08d4b4ec29e
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-06-24 21:47:41 +0200

    change win32 button style

--- Forms/Sources/__OS/ButtonWin32.cpp
+++ Forms/Sources/__OS/ButtonWin32.cpp
@@ -15,7 +15,7 @@ using namespace __OS;
 extern HINSTANCE __instance;
 
 intptr FormsApi::Button::Create(const System::Windows::Forms::Button& button) {
-  HWND handle = CreateWindowEx(0, WC_BUTTON, button.Text().w_str().c_str(), WS_CHILD | BS_PUSHBUTTON, button.Left, button.Top, button.Width, button.Height, (HWND)button.Parent()().Handle(), (HMENU)0, __instance, (LPVOID)NULL);
+  HWND handle = CreateWindowEx(0, WC_BUTTON, button.Text().w_str().c_str(), WS_CHILD | BS_PUSHBUTTON | BS_MULTILINE | BS_CENTER | BS_VCENTER, button.Left, button.Top, button.Width, button.Height, (HWND)button.Parent()().Handle(), (HMENU)0, __instance, (LPVOID)NULL);
   WindowProcedure::SetWindowTheme(handle);
   WindowProcedure::DefWindowProcs[(intptr)handle] = (WNDPROC)SetWindowLongPtr(handle, GWLP_WNDPROC, (LONG_PTR)WindowProcedure::WndProc);
   /// @todo to remove after create SetFont method...
