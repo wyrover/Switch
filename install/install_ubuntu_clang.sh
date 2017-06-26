0403270a3786f58785bc2e5061d0c7ecf0a7ea1c
yfiumefreddo <y.fiumefredddo@gmail.com>
2017-06-26 22:06:38 +0200

    fix install Ubuntu

--- install/install_ubuntu_clang.sh
+++ install/install_ubuntu_clang.sh
@@ -1,33 +1,30 @@
 echo "Install Pcf libraries version 0.2.0, copyright GAMMA Soft, 2017"
 echo ""
 
-rm  -r Build
-sudo mkdir /usr/local/cmake
-sudo chmod 777 /usr/local/bin
-sudo chmod 777 /usr/local/cmake
-sudo chmod 777 /usr/local/include
-sudo chmod 777 /usr/local/lib
+rm  -r -q Build
 mkdir -p Build/Examples
 mkdir -p Build/ThirdParties
 
 sudo apt update
-sudo apt install clang cmake doxygen libcurlpp-dev libgtkmm-3.0-dev libjpeg8-dev libpng-dev libssl-dev libx11-dev uuid-dev zlibc
+sudo apt install clang cmake doxygen libcurlpp-dev libgtkmm-3.0-dev libjpeg8-dev libpng-dev libssl-dev libx11-dev uuid-dev zlibc -y
 
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
 cd Build/ThirdParties
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../../ThirdParties 
-make install -j6
+make all -j6
+sudo make install
 
 cd ..
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
 make documentation -j6
-xdg-open Help/html/index.html &
-make install -j6
+xdg-open Help/html/index.html &>/dev/null &
+make all -j6
+sudo make install
 
 cd Examples
 #cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../../Examples
-#make -j6
+#make all -j6
 cd ../..
 
