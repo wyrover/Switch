5ed40ccf9972d61cb2784a8a06431baa5c8baf8f
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-06-29 23:00:27 +0200

    Add TrackBar control

--- Forms/ManualTests/Main.cpp
+++ Forms/ManualTests/Main.cpp
@@ -21,7 +21,7 @@ namespace PcfFormApp {
       Label label1;
       label1.Location = Point(50, 50);
       label1.Text = "0";
-      
+
       TrackBar trackBar1;
       trackBar1.Location = Point(50, 100);
       trackBar1.Width = 200;
@@ -39,11 +39,11 @@ namespace PcfFormApp {
       trackBar2.Orientation = Orientation::Vertical;
       trackBar2.Location = Point(50, 150);
       trackBar2.Size = Size(45, 104);
-      trackBar2.TickFrequency = 1;
+      trackBar2.TickFrequency = 2;
       trackBar2.ValueChanged += pcf_delegate(const object& sender, const EventArgs& e) {
         label2.Text = string::Format("{0}", trackBar2.Value);
       };
-      
+
       Form form;
       form.Controls().AddRange({label1, trackBar1, label2, trackBar2});
 
