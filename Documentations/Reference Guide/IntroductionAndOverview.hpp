7d1d51419edec2f6788c4b655f757e59c02bb3d3
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-07-19 21:44:19 +0200

    add comments

--- Documentations/Reference Guide/IntroductionAndOverview.hpp
+++ Documentations/Reference Guide/IntroductionAndOverview.hpp
@@ -1,36 +1,11 @@
 /// @page IntroductionAndOverviewPage Introduction and Overview
 ///
 /// The Pcf Libraries are...
-///   * a collection of C++ classes libraries, similar in concept to the .NET Framework;
-///   * focused on "internet-age" network-centric applications;
-///   * written in efficient, modern ANSI/ISO Standard C++ and based on the C++ Standard Library/STL;
+///   * a collection of C++ classes libraries, similar to the .NET Framework;
+///   * written in efficient, modern C++14;
 ///   * and highly portable and available on many different platforms;
 ///
 /// @image HTML Pcf.png
-///
-/// @section FeaturesSection Features
-///  * Cache framework
-///  * Date and Time classes
-///  * Delegates for events (signal/slot mechanism) and notifications
-///  * FTP client for transferring files
-///  * Filesystem classes for platform-independent path manipulation, directory listing and globing
-///  * HTTP server and client (also secure), C++ Server Page Compiler
-///  * Logging
-///  * Multithreading: basic threads and synchronization and advanced facilities (thread pool, active objects, work queues, etc.)
-///  * Platform Abstraction: write once, compile and run on multiple platforms
-///  * Processes and IPC
-///  * Reactor framework
-///  * SQL database access (SQLite)
-///  * Smart pointers and memory management (buffer, pool)
-///  * Sockets and raw sockets
-///  * Stream classes for Base64 and HexBinary encoding/decoding, compression (zlib), line ending conversion, reading/writing to memory, etc.
-///  * String formatting and string utilities
-///  * TCP server framework (multithreaded)
-///  * Text encodings and conversions
-///  * Tuples
-///  * URI handling
-///  * UTF-8, UTF-16, UTF-32 and Unicode support
-///  * UUID handling and generation ￼￼
 ///
 /// @section ObjectivesAndMissionSection Objectives and Mission
 ///  * Pcf is a powerful, yet easy to use platform to build your applications upon
