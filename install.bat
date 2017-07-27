f5654ce2f79fb79e48be8909288c0969ecddded4
yfiumefreddo <y.fiumefreddo@gmail.com>
2017-07-27 12:36:57 +0200

    fiw build windows and add visual 2017

--- install.bat
+++ install.bat
@@ -1,2 +1,6 @@
 @echo off
-call install\install_windows_visual_studio_2015.bat
+if "%1" == "2017" (
+  call install\install_windows_visual_studio_2017.bat 
+) else if "%1" == "2015" (
+  call install\install_windows_visual_studio_2015.bat 
+) else echo "param can only be 2017 or 2015" 
---
+++ install/install_windows_visual_studio_2017.bat
@@ -0,0 +1,27 @@
+@echo off
+echo "Install Pcf libraries version 0.3.0, copyright GAMMA Soft, 2017"
+echo ""
+
+del /q /s Build
+mkdir Build\Examples
+mkdir Build\ThirdParties
+mkdir c:\usr\local
+
+cd Build\ThirdParties
+cmake -G "Visual Studio 15 2017 Win64" -D CMAKE_INSTALL_PREFIX:STRING="C:/usr/local" ../../ThirdParties
+"devenv" "ThirdParties.sln" /project install /build Debug
+"devenv" "ThirdParties.sln" /project install /build Release
+
+cd..
+cmake -G "Visual Studio 15 2017 Win64" -D CMAKE_INSTALL_PREFIX:STRING="C:/usr/local" ..
+"devenv" "Pcf.sln" /project documentation /build Debug
+start Help/html/index.html
+"devenv" "Pcf.sln" /project install /build Debug
+"devenv" "Pcf.sln" /project install /build Release
+
+cd Examples
+cmake -G "Visual Studio 15 2017 Win64" -D CMAKE_INSTALL_PREFIX:STRING="C:/usr/local" ../../Examples
+rem "devenv" "Examples.sln" /project ALL_BUILD /build Release
+start Examples.sln
+cd ..\..
+
