026719b6629631111239c6d1185f7ba9cbc6a73c
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-07-28 23:46:07 +0200

    add Example DesignPattern Visitor

---
+++ Examples/DesignPatterns/Visitor/Visitor.cpp
@@ -0,0 +1,124 @@
+// Visitor pattern -- Behavorail example
+
+#include <Pcf/Pcf>
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+namespace DesignPatterns {
+  namespace Behavioral {
+    class Visitor;
+    
+    // The 'Element' abstract class
+    class Element pcf_abstract {
+    public:
+      virtual void Accept(refptr<Visitor> visitor) = 0;
+    };
+    
+    // A 'ConcreteElement' class
+    class ConcreteElementA : public Element {
+    public:
+      void Accept(refptr<Visitor> visitor) override;
+      
+      void OperationA() {
+      }
+    };
+    
+    // A 'ConcreteElement' class
+    class ConcreteElementB : public Element {
+    public:
+      void Accept(refptr<Visitor> visitor) override;
+      
+      void OperationB() {
+      }
+    };
+    
+    // The 'Visitor' abstract class
+    class Visitor pcf_abstract {
+    public:
+      virtual void VisitConcreteElementA(const ConcreteElementA& concreteElementA) = 0;
+      virtual void VisitConcreteElementB(const ConcreteElementB& concreteElementB) = 0;
+    };
+
+    inline void ConcreteElementA::Accept(refptr<Visitor> visitor) {
+      visitor->VisitConcreteElementA(*this);
+    }
+
+    inline void ConcreteElementB::Accept(refptr<Visitor> visitor) {
+      visitor->VisitConcreteElementB(*this);
+    }
+    
+    // A 'ConcreteVisitor' class
+    class ConcreteVisitor1 : public Visitor {
+    public:
+      void VisitConcreteElementA(const ConcreteElementA& concreteElementA) override {
+        Console::WriteLine("{0} visited by {1}", concreteElementA.GetType().Name, this->GetType().Name);
+      }
+      
+      void VisitConcreteElementB(const ConcreteElementB& concreteElementB) override {
+        Console::WriteLine("{0} visited by {1}", concreteElementB.GetType().Name, this->GetType().Name);
+      }
+    };
+    
+    /// A 'ConcreteVisitor' class
+    class ConcreteVisitor2 : public Visitor {
+    public:
+      void VisitConcreteElementA(const ConcreteElementA& concreteElementA) override {
+        Console::WriteLine("{0} visited by {1}", concreteElementA.GetType().Name, this->GetType().Name);
+      }
+      
+      void VisitConcreteElementB(const ConcreteElementB& concreteElementB) override {
+        Console::WriteLine("{0} visited by {1}", concreteElementB.GetType().Name, this->GetType().Name);
+      }
+    };
+    
+    // The 'ObjectStructure' class
+    class ObjectStructure : public object {
+    public:
+      void Attach(refptr<Element> element) {
+        this->elements.Add(element);
+      }
+      
+      void Detach(refptr<Element> element) {
+        this->elements.Remove(element);
+      }
+      
+      void Accept(refptr<Visitor> visitor) {
+        for (refptr<Element> element : this->elements) {
+          element->Accept(visitor);
+        }
+      }
+    private:
+      List<refptr<Element>> elements;
+    };
+
+    // MainApp startup class for Structural
+    // Visitor Design Pattern.
+    class MainApp {
+    public:
+      static void Main() {
+        // Setup structure
+        ObjectStructure o;
+        o.Attach(pcf_new<ConcreteElementA>());
+        o.Attach(pcf_new<ConcreteElementB>());
+        
+        // Create visitor objects
+        refptr<ConcreteVisitor1> v1 = pcf_new<ConcreteVisitor1>();
+        refptr<ConcreteVisitor2> v2 = pcf_new<ConcreteVisitor2>();
+        
+        // Structure accepting visitors
+        o.Accept(v1);
+        o.Accept(v2);
+      }
+    };
+  }
+}
+
+pcf_startup (DesignPatterns::Behavioral::MainApp)
+
+// This code produces the following output:
+//
+// ConcreteElementA visited by ConcreteVisitor1
+// ConcreteElementB visited by ConcreteVisitor1
+// ConcreteElementA visited by ConcreteVisitor2
+// ConcreteElementB visited by ConcreteVisitor2
