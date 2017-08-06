444971128c9d7adec8d97287e82e337b0845c6f2
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-08-06 22:18:07 +0200

    change install parameters on windows

--- install/install_windows_usage.bat
+++ install/install_windows_usage.bat
@@ -1,11 +1,11 @@
 @echo off
 echo   usage Install [/VS:2017 ^| /VS:2015]
-echo       /VS:2017 : Visual Studio 2017 (default)
+echo       /VS:2017 : Visual Studio 2017 (default is no specified param)
 echo       /VS:2015 : Visual Studio 2015
 echo.
 echo Notes
-echo   * By default the cmake variable CMAKE_INSTALL_PREFIX is set to 
-echo     "C:/usr/local".
+echo   * The Pcf installer set cmake variable CMAKE_INSTALL_PREFIX to 
+echo     "C:/usr/local" for generate, build and install Pcf.
 echo   * When install is complete you can find installed Pcf files in folder
 echo     "C:/usr/local".
 echo   * To build your own project using Pcf, don't miss to set cmake variable
