8c3a346c4d2af7dc9e72bc301f668696203121f7
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-08-07 23:10:08 +0200

    change cmake file

--- install/install_macosx_xcode.sh
+++ install/install_macosx_xcode.sh
@@ -6,7 +6,7 @@ mkdir -p Build/ThirdParties
 
 brew update
 brew upgrade
-brew install cmake curl doxygen fltk gtkmm3 jpeg libpng zlib
+brew install cmake curl doxygen jpeg libpng zlib
 
 cd Build/ThirdParties
 cmake -G "Xcode" ../../ThirdParties
