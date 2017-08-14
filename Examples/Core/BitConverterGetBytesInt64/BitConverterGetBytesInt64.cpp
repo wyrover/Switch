d30d5be0d50296fb5dee545322e081956dcede7f
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-08-14 22:59:08 +0200

    add BitConverter Examples

---
+++ Examples/Core/BitConverterGetBytesInt64/BitConverterGetBytesInt64.cpp
@@ -0,0 +1,44 @@
+#include <Pcf/Pcf>
+
+using namespace System;
+
+namespace Examples {
+  class Program {
+  public:
+    // The main entry point for the application.
+    static void Main() {
+      // Define an array of Int64 values.
+      Array<int64> values = {0, 0xFFFFFF, -0xFFFFFF, 1000000000, -1000000000, 0x100000000, -0x100000000, 0xAAAAAAAAAAAA, -0xAAAAAAAAAAAA, 1000000000000000000, -1000000000000000000, Int64::MinValue, Int64::MaxValue};
+      
+      Console::WriteLine( "{0,22}{1,10}{2,30}", "Int64", "Endian", "Byte Array");
+      Console::WriteLine( "{0,22}{1,10}{2,30}", "-----", "------", "----------" );
+      
+      for (auto value : values) {
+        // Convert each Int64 value to a byte array.
+        Array<byte> byteArray = BitConverter::GetBytes(value);
+        // Display the result.
+        Console::WriteLine("{0,22}{1,10}{2,30}", value, BitConverter::IsLittleEndian ? "Little" : "Big", BitConverter::ToString(byteArray));
+      }
+    }
+  };
+}
+
+pcf_startup (Examples::Program)
+
+// This code produces the following output:
+//
+//                  Int64    Endian                    Byte Array
+//                  -----    ------                    ----------
+//                      0    Little       00-00-00-00-00-00-00-00
+//               16777215    Little       FF-FF-FF-00-00-00-00-00
+//              -16777215    Little       01-00-00-FF-FF-FF-FF-FF
+//             1000000000    Little       00-CA-9A-3B-00-00-00-00
+//            -1000000000    Little       00-36-65-C4-FF-FF-FF-FF
+//             4294967296    Little       00-00-00-00-01-00-00-00
+//            -4294967296    Little       00-00-00-00-FF-FF-FF-FF
+//        187649984473770    Little       AA-AA-AA-AA-AA-AA-00-00
+//       -187649984473770    Little       56-55-55-55-55-55-FF-FF
+//    1000000000000000000    Little       00-00-64-A7-B3-B6-E0-0D
+//   -1000000000000000000    Little       00-00-9C-58-4C-49-1F-F2
+//   -9223372036854775808    Little       00-00-00-00-00-00-00-80
+//    9223372036854775807    Little       FF-FF-FF-FF-FF-FF-FF-7F
---
+++ Examples/Core/BitConverterGetBytesSingle/BitConverterGetBytesSingle.cpp
@@ -0,0 +1,72 @@
+#include <Pcf/Pcf>
+
+using namespace System;
+
+namespace Examples {
+  class Program {
+  public:
+    static const string formatter;
+    
+    // Convert a double argument to a byte array and display it.
+    static void GetBytesSingle(float argument) {
+      Array<byte> byteArray = BitConverter::GetBytes(argument);
+      Console::WriteLine(formatter, argument, BitConverter::ToString(byteArray));
+    }
+
+    // The main entry point for the application.
+    static void Main() {
+      Console::WriteLine("This example of the BitConverter.GetBytes(float) "
+                         "\nmethod generates the following output.\n");
+      Console::WriteLine(formatter, "float", "byte array");
+      Console::WriteLine(formatter, "-----", "----------");
+      
+      // Convert double values and display the results.
+      GetBytesSingle(0.0F);
+      GetBytesSingle(1.0F);
+      GetBytesSingle(15.0F);
+      GetBytesSingle(65535.0F);
+      GetBytesSingle(0.00390625F);
+      GetBytesSingle(0.00000000023283064365386962890625F);
+      GetBytesSingle(1.2345E-35F);
+      GetBytesSingle(1.2345671F);
+      GetBytesSingle(1.2345673F);
+      GetBytesSingle(1.2345677F);
+      GetBytesSingle(1.23456789E+35F);
+      GetBytesSingle(Single::MinValue);
+      GetBytesSingle(Single::MaxValue);
+      GetBytesSingle(Single::Epsilon);
+      GetBytesSingle(Single::NaN);
+      GetBytesSingle(Single::NegativeInfinity);
+      GetBytesSingle(Single::PositiveInfinity);
+    }
+  };
+  
+  const string Program::formatter = "{0,16:E7}{1,20}";
+}
+
+pcf_startup (Examples::Program)
+
+// This code produces the following output:
+//
+// This example of the BitConverter.GetBytes(float)
+// method generates the following output.
+//
+//            float          byte array
+//            -----          ----------
+//   0.0000000E+000         00-00-00-00
+//   1.0000000E+000         00-00-80-3F
+//   1.5000000E+001         00-00-70-41
+//   6.5535000E+004         00-FF-7F-47
+//   3.9062500E-003         00-00-80-3B
+//   2.3283064E-010         00-00-80-2F
+//   1.2344999E-035         49-46-83-05
+//   1.2345670E+000         4B-06-9E-3F
+//   1.2345673E+000         4D-06-9E-3F
+//   1.2345676E+000         50-06-9E-3F
+//   1.2345679E+035         1E-37-BE-79
+//  -3.4028235E+038         FF-FF-7F-FF
+//   3.4028235E+038         FF-FF-7F-7F
+//   1.4012985E-045         01-00-00-00
+//              NaN         00-00-C0-7F
+//        -Infinity         00-00-80-FF
+//         Infinity         00-00-80-7F
