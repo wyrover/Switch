36087e92f21b368dd466172aaaf6e77886cc1bb1
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-08-27 13:37:49 +0200

    add examples

--- Examples/TUnit/Assert/Assert.cpp
+++ Examples/TUnit/Assert/Assert.cpp
@@ -15,13 +15,6 @@ namespace UnitTests {
       Assert::AreEqual(15, ts.Minutes, pcf_current_information);
     }
 
-    // Used Assert::Throws to verifie if method throw effectively an exception
-    void CreateTimeSpanFromNullTimeSpan() {
-      Assert::Throws<ArgumentNullException>(pcf_delegate {
-        TimeSpan timeSpan(Ref<TimeSpan>::Null());
-      }, pcf_current_information);
-    }
-
     // Used Assert::IsTrue to virifie if a condition is true
     void TimeSpanIsEqualToAnotherTimeSpan() {
       TimeSpan ts(10, 42, 24);
@@ -36,21 +29,19 @@ namespace UnitTests {
 
   // Used pcf_test to add unit test to execute at the unit test suit.
   pcf_test(TimeSpanTest, CreateTimeSpanFromDateTime);
-  pcf_test(TimeSpanTest, CreateTimeSpanFromNullTimeSpan);
   pcf_test(TimeSpanTest, TimeSpanIsEqualToAnotherTimeSpan);
   pcf_test(TimeSpanTest, DefaultTimeSpanIsEqualToZero);
 }
 
 // This code produces the following output:
 //
-// Start 4 tests from 1 test case
-//   Start 4 tests from TimeSpanTest
+// Start 3 tests from 1 test case
+//   Start 3 tests from TimeSpanTest
 //     PASSED TimeSpanTest.CreateTimeSpanFromDateTime (1 ms)
-//     PASSED TimeSpanTest.CreateTimeSpanFromNullTimeSpan (7 ms)
 //     PASSED TimeSpanTest.TimeSpanIsEqualToAnotherTimeSpan (0 ms)
 //     PASSED TimeSpanTest.DefaultTimeSpanIsEqualToZero (0 ms)
-//   End 4 tests from TimeSpanTest (45 ms total)
+//   End 3 tests from TimeSpanTest (4 ms total)
 //
 //   Summary :
-//     PASSED 4 tests.
-// End 4 tests from 1 test case ran. (45 ms total)
+//     PASSED 3 tests.
+// End 3 tests from 1 test case ran. (5 ms total)
---
+++ Examples/TUnit/HelloWorldTest/HelloWorldTest.cpp
@@ -0,0 +1,34 @@
+#include <Pcf/Pcf>
+
+using namespace System;
+using namespace TUnit;
+
+namespace UnitTests {
+  class HelloWorldTest : public TestFixture {
+  protected:
+    void CreateStringFromLiteral() {
+      string s = "Hello, World!";
+      Assert::AreEqual("Hello, World!", s);
+    }
+    
+    void CreateStringFromChar32Array() {
+      string s = string(Array<char32> {'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!'});
+      Assert::AreEqual("Hello, World!", s);
+    }
+  };
+  
+  pcf_test (HelloWorldTest, CreateStringFromLiteral)
+  pcf_test (HelloWorldTest, CreateStringFromChar32Array)
+}
+
+//
+//
+// Start 2 tests from 1 test case
+//   Start 2 tests from HelloWorldTest
+//     PASSED HelloWorldTest.CreateStringFromLiteral (0 ms)
+//     PASSED HelloWorldTest.CreateStringFromChar32Array (0 ms)
+//   End 2 tests from HelloWorldTest (3 ms total)
+//
+//   Summary :
+//     PASSED 2 tests.
+// End 2 tests from 1 test case ran. (4 ms total)
