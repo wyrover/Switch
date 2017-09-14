f4421561a5d2f74e0c447466ca650e921d544100
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-09-14 16:38:48 +0200

    add examples

---
+++ Examples/TUnit/TestFixtureMain2/README.md
@@ -0,0 +1,17 @@
+# TestFixture
+This code example shows how to use your own main entry point method.
+
+## Remarks
+To add your own main entry point, you must link with Switch.TUnit nor Switch.TUnit.Main. (see CMakeLists.txt)
+
+## Output
+```
+Start 1 test from 1 test case
+  Start 1 test from UserTest
+    PASSED UserTest.AnyTest (1 ms)
+  End 1 test from UserTest (3 ms total)
+
+  Summary :
+    PASSED 1 test.
+End 1 test from 1 test case ran. (3 ms total)
+```
---
+++ Examples/TUnit/TestFixtureMain2/TestFixtureMain2.cpp
@@ -0,0 +1,38 @@
+// Remarks : To add your own main entry point, you must link with Switch.TUnit nor Switch.TUnit.Main. (see CMakeLists.txt)
+
+#include <Switch/Switch>
+
+using namespace System;
+using namespace TUnit;
+
+namespace UnitTests {
+  // The class DirectoryTest must be inherited from TestFixture
+  class UserTest : public TestFixture {
+  protected:
+    void AnyTest() {
+      Assert::Null(null, _caller);
+    }
+  };
+  
+  // Used _test to add unit test to execute at the unit test suit.
+  _test(UserTest, AnyTest);
+}
+
+// The main entry point for the test application.
+int main(int argc, char* argv[]) {
+  // You can add your own code here.
+  
+  // Create UnitTest and run it
+  return TUnit::UnitTest(Environment::SetCommandLineArgs(argv, argc)).Run();
+}
+
+// This code produces the following output if directory Downloads exists and Potatos doesn't exist in your home direcory:
+//
+// Start 1 test from 1 test case
+//   Start 1 test from UserTest
+//     PASSED UserTest.AnyTest (1 ms)
+//   End 1 test from UserTest (3 ms total)
+//
+//   Summary :
+//     PASSED 1 test.
+// End 1 test from 1 test case ran. (3 ms total)
