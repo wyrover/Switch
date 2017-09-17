d2d7a76d5b5a5e2280d1cd50ffa5a27ed6319b99
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-09-17 22:16:41 +0200

    add Examples

---
+++ Examples/Core/StringFormatBinary/StringFormatBinary.cpp
@@ -0,0 +1,19 @@
+#include <Switch/Switch>
+
+using namespace System;
+
+namespace Examples {
+  class Program {
+  public:
+    // The main entry point for the application.
+    static void Main() {
+      Console::WriteLine("Value of 165 in binary = 0b{0:B16}.", 0b10100101);
+    }
+  };
+}
+
+_startup (Examples::Program)
+
+// This code produces the following output:
+//
+// Value of 165 in binary = 0b0000000010100101.
