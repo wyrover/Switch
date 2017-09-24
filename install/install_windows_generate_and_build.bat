ca003d5fdf189a8f854ea176b951af3230d63c67
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-09-24 19:55:52 +0200

    fix windows install

--- install/install_windows_generate_and_build.bat
+++ install/install_windows_generate_and_build.bat
@@ -1,26 +1,29 @@
 set cmake_install_prefix_path=C:/usr/local
-if not "%2" == "" set cmake_install_prefix_path=%2
+if not "%3" == "" set cmake_install_prefix_path=%3
+
+rem echo cmake %1 %2 -DCMAKE_INSTALL_PREFIX:STRING="%cmake_install_prefix_path%" ../../ThirdParties
+rem goto end
 
 del /q /s bin
 mkdir bin\Examples
 mkdir bin\ThirdParties
-mkdir c:\usr\local
+mkdir "%cmake_install_prefix_path%"
 
 cd bin\ThirdParties
-cmake "%1" -DCMAKE_INSTALL_PREFIX:STRING="%cmake_install_prefix_path%" ../../ThirdParties
+cmake %1 %2 -DCMAKE_INSTALL_PREFIX:STRING="%cmake_install_prefix_path%" ../../ThirdParties
 "devenv" "ThirdParties.sln" /project install /build Debug
-"devenv" "ThirdParties.sln" /project install /build Release
+rem "devenv" "ThirdParties.sln" /project install /build Release
 
 cd..
-cmake "%1" -DCMAKE_INSTALL_PREFIX:STRING="%cmake_install_prefix_path%" ..
-"devenv" "Switch.sln" /project documentation /build Debug
-start Help/html/index.html
+cmake %1 %2 -DCMAKE_INSTALL_PREFIX:STRING="%cmake_install_prefix_path%" ..
+rem "devenv" "Switch.sln" /project documentation /build Debug
+rem start Help/html/index.html
 "devenv" "Switch.sln" /project install /build Debug
-"devenv" "Switch.sln" /project install /build Release
+rem "devenv" "Switch.sln" /project install /build Release
 
 cd Examples
-cmake "%1" -DCMAKE_INSTALL_PREFIX:STRING="%cmake_install_prefix_path%" ../../Examples
-rem "devenv" "Examples.sln" /project ALL_BUILD /build Release
+cmake %1 %2 -DCMAKE_INSTALL_PREFIX:STRING="%cmake_install_prefix_path%" ../../Examples
 start Examples.sln
 cd ..\..
 
+rem :end
