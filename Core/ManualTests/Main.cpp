0e6bf6bfdfe714a8a5ee4a1b7a38975182a607a0
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-10-02 10:51:49 +0200

    fix build

--- Core/ManualTests/Main.cpp
+++ Core/ManualTests/Main.cpp
@@ -14,17 +14,6 @@ namespace Examples {
   public:
     // The main entry point for the application.
     static void Main() {
-      enum class Number {Nine = 9};
-      Write("1", " ", "2"_s, " ", 3, " ", 4_s, " ", .5, " ", Version(6, 0), " ", Net::IPAddress(7), " " , '8', " ", Number::Nine);
-      Console::WriteLine();
-    }
-    
-    static void Write() {}
-
-    template<typename Arg, typename ...Args>
-    static void Write(const Arg& arg, const Args& ...args) {
-      Console::Write(arg);
-      Write(args...);
     }
   };
 }
