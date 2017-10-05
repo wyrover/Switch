cf65c046931e513c9734adb0e9be4ce2ea945a5d
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-10-05 15:48:30 +0200

    projects review

---
+++ tests/Switch.System.Drawing.ManualTests/Main.cpp
@@ -0,0 +1,21 @@
+#define TRACE
+#include <Switch/Startup.hpp>
+#include <Switch/System/Console.hpp>
+#include <Switch/System/Net/IPAddress.hpp>
+#include <Switch/System/Threading/Monitor.hpp>
+#include <Switch/System/Threading/Thread.hpp>
+#include <Switch/System/Threading/Tasks/Task.hpp>
+
+using namespace System;
+using namespace System::Threading;
+
+namespace Examples {
+  class Program {
+  public:
+    // The main entry point for the application.
+    static void Main() {
+    }
+  };
+}
+
+_startup (Examples::Program)
