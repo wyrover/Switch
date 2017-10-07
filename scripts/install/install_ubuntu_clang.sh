38efd7ef8e5634d60ae87e0214024284ad1c4e91
yfiumefreddo <y.fiumefreddo@gmail.com>
2017-10-07 18:29:50 +0200

    fix ubuntu install

--- scripts/install/install_ubuntu_clang.sh
+++ scripts/install/install_ubuntu_clang.sh
@@ -1,33 +1,32 @@
-rm  -r -q bin
-mkdir -p bin/Examples
-mkdir -p bin/3rdparty
-
 sudo apt update
 sudo apt install clang cmake doxygen libcurlpp-dev libgtkmm-3.0-dev libjpeg8-dev libpng-dev libssl-dev libx11-dev uuid-dev zlibc -y
 
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
+rm  -r -q bin
+mkdir -p bin/3rdparty
 cd bin/3rdparty
-cmake -G "Unix Makefiles" -DCMAKE_bin_TYPE=Debug ../../3rdparty
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ../../3rdparty
 make all -j6
 sudo make install
-cmake -G "Unix Makefiles" -DCMAKE_bin_TYPE=Release ../../3rdparty
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../../3rdparty
 make all -j6
 sudo make install
 
 cd ..
-cmake -G "Unix Makefiles" -DCMAKE_bin_TYPE=Debug ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
 #make documentation -j6
 #xdg-open Help/html/index.html &>/dev/null &
 make all -j6
 sudo make install
-cmake -G "Unix Makefiles" -DCMAKE_bin_TYPE=Release ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
 make all -j6
 sudo make install
 
-cd Examples
-#cmake -G "Unix Makefiles" -DCMAKE_bin_TYPE=Debug ../../Examples
-cmake -G "Unix Makefiles" -DCMAKE_bin_TYPE=Release ../../Examples
+mkdir examples
+cd examples
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ../../examples
+#cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../../examples
 cd ../..
 
