073aed44a217d59c97c33b6586c087f266fd6ff6
gammasoft71 <gammasoft71@gmail.com>
2017-10-20 13:41:23 +0200

    fix linux install

--- scripts/install/install_macosx_xcode.sh
+++ scripts/install/install_macosx_xcode.sh
@@ -26,14 +26,11 @@ fi
 mkdir -p build/3rdparty
 cd build/3rdparty
 cmake -G "Xcode" ../../3rdparty
-xcodebuild -target all -configuration Release
 xcodebuild -target install -configuration Release
 
 cd ..
 cmake -G "Xcode" ..
-xcodebuild -target all -configuration Debug
 xcodebuild -target install -configuration Debug
-xcodebuild -target all -configuration Release
 xcodebuild -target install -configuration Release
 
 mkdir examples
