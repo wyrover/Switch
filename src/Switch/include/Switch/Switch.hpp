5b4b590fc114fa823a8aea56637493e8d72d2198
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-10-30 10:49:05 +0100

    add Switch.System.ServiceModel library

--- src/Switch/include/Switch/Switch.hpp
+++ src/Switch/include/Switch/Switch.hpp
@@ -4,5 +4,6 @@
 #include "Switch/Switch.System"
 #include "Switch/Switch.System.Core"
 #include "Switch/Switch.System.Drawing"
+#include "Switch/Switch.System.ServiceModel"
 #include "Switch/Switch.System.Windows.Forms"
 #include "Switch/Switch.TUnit.Core"
