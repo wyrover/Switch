11ee38676cb442c3b6443a9c8a53b94e24473f6f
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-11-06 00:39:12 +0100

    use GTest

--- scripts/cmake/MakeTestProject.cmake
+++ scripts/cmake/MakeTestProject.cmake
@@ -8,7 +8,8 @@ macro(MakeTestProject)
   GroupSources(.)
 
   add_executable(${PROJECT_NAME} ${SOURCE_FILES})
-  target_link_libraries(${PROJECT_NAME} Switch.TUnit.Main ${LINK_LIBRARIES})
+  #target_link_libraries(${PROJECT_NAME} Switch.TUnit.Main ${LINK_LIBRARIES})
+  target_link_libraries(${PROJECT_NAME} ${GTEST_LIBRARIES} ${LINK_LIBRARIES})
   set_target_properties(${PROJECT_NAME} PROPERTIES FOLDER tests)
 
   add_test(NAME ${PROJECT_NAME} COMMAND $<TARGET_FILE_NAME:${PROJECT_NAME}> --tunit_filter=*.* --tunit_output=xml:${PROJECT_NAME}.xml WORKING_DIRECTORY $<TARGET_FILE_DIR:${PROJECT_NAME}>)
--- src/Switch.TUnit.Core/include/Switch/TUnit/AssertionResult.hpp
+++ src/Switch.TUnit.Core/include/Switch/TUnit/AssertionResult.hpp
@@ -2,33 +2,18 @@
 /// @brief Contains Switch::TUnit::AssertionResult class.
 #pragma once
 
-#include <cstring>
-#include <iostream>
-
-#include <Switch/Types.hpp>
-#include <Switch/System/Object.hpp>
 #include <Switch/System/String.hpp>
-
-/// @cond
-namespace testing {
-  class AssertionResult;
-  namespace internal {
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual);
-    AssertionResult CmpHelperNE(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual);
-    AssertionResult CmpHelperLE(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual);
-    AssertionResult CmpHelperLT(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual);
-    AssertionResult CmpHelperGE(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual);
-    AssertionResult CmpHelperGT(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual);
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, char expected, const char32& actual);
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, int expected, const char32& actual);
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, const System::String& expected, const System::String& actual);
-    AssertionResult CmpHelperNE(const char* expectedExpression, const char* actualExpression, const System::String& expected, const System::String& actual);
-  }
-}
-
 #include <gtest/gtest.h>
 
-::testing::AssertionResult AssertObjectEqStr(const char* expectedExpression,const char* actualExpression,const System::String& expected,const char* actual);
-::testing::AssertionResult AssertObjectEqStr(const char* expectedExpression,const char* actualExpression,const char* expected,const System::String& actual);
+namespace testing {
+  namespace internal {
+    inline AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual) {return CmpHelperEQ(expectedExpression, actualExpression, expected, std::string(actual.c_str()));}
+    inline AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, const System::String& expected, const System::String& actual) {return CmpHelperEQ(expectedExpression, actualExpression, std::string(expected.c_str()), std::string(actual.c_str()));}
+    inline AssertionResult CmpHelperNE(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual) {return CmpHelperNE(expectedExpression, actualExpression, expected, std::string(actual.c_str()));}
+    inline AssertionResult CmpHelperNE(const char* expectedExpression, const char* actualExpression, const System::String& expected, const System::String& actual) {return CmpHelperNE(expectedExpression, actualExpression, std::string(expected.c_str()), std::string(actual.c_str()));}
+    inline AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, char expected, const char32& actual) {return CmpHelperEQ(expectedExpression, actualExpression, expected, (char)actual);}
+    inline AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, int expected, const char32& actual) {return CmpHelperEQ(expectedExpression, actualExpression, expected, (int)actual);}
+  }
+}
 
 /// @endcond
