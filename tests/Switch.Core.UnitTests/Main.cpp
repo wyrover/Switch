8a162d814969b94ef9dcae5b60baae82cd4ff619
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-11-07 19:15:27 +0100

    use GTest

--- tests/Switch.Core.UnitTests/Main.cpp
+++ tests/Switch.Core.UnitTests/Main.cpp
@@ -1,13 +1,5 @@
 #include <Switch/System/Environment.hpp>
-#include <Switch/System/String.hpp>
 #include <gtest/gtest.h>
-
-namespace testing {
-  namespace internal {
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, char expected, const char32& actual) {return CmpHelperEQ(expectedExpression, actualExpression, expected, (char)actual);}
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, int expected, const char32& actual) {return CmpHelperEQ(expectedExpression, actualExpression, expected, (int)actual);}
-  }
-}
 
 int main(int argc, char* argv[]) {
   System::Environment::SetCommandLineArgs(argv, argc);
--- tests/Switch.System.Drawing.UnitTests/Main.cpp
+++ tests/Switch.System.Drawing.UnitTests/Main.cpp
@@ -1,18 +1,8 @@
-#include <Switch/System/String.hpp>
+#include <Switch/System/Environment.hpp>
 #include <gtest/gtest.h>
 
-namespace testing {
-  namespace internal {
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual) {return CmpHelperEQ(expectedExpression, actualExpression, expected, std::string(actual.c_str()));}
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, const System::String& expected, const System::String& actual) {return CmpHelperEQ(expectedExpression, actualExpression, std::string(expected.c_str()), std::string(actual.c_str()));}
-    AssertionResult CmpHelperNE(const char* expectedExpression, const char* actualExpression, const char* expected, const System::String& actual) {return CmpHelperNE(expectedExpression, actualExpression, expected, std::string(actual.c_str()));}
-    AssertionResult CmpHelperNE(const char* expectedExpression, const char* actualExpression, const System::String& expected, const System::String& actual) {return CmpHelperNE(expectedExpression, actualExpression, std::string(expected.c_str()), std::string(actual.c_str()));}
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, char expected, const char32& actual) {return CmpHelperEQ(expectedExpression, actualExpression, expected, (char)actual);}
-    AssertionResult CmpHelperEQ(const char* expectedExpression, const char* actualExpression, int expected, const char32& actual) {return CmpHelperEQ(expectedExpression, actualExpression, expected, (int)actual);}
-  }
-}
-
 int main(int argc, char* argv[]) {
+  System::Environment::SetCommandLineArgs(argv, argc);
   System::Exception::StackTraceEnabled(false);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
