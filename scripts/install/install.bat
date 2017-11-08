fb07c9ad6d7bc437c9c69d17d0f0204e533a909b
gammasoft71 <gammasoft71@gmail.com>
2017-11-08 13:02:55 +0100

    review install on windows

--- scripts/install/install.bat
+++ scripts/install/install.bat
@@ -31,21 +31,36 @@ if "%1" == "" (
 
 if "%switch_install_cmake_install_prefix_path%" == "" set switch_install_cmake_install_prefix_path=C:/usr/local
 
-if "%switch_install_option%" == "/VCPKG" (
-  "%switch_install_vcpkg_path%\vcpkg" install curl gtest libjpeg-turbo zlib libpng
-  "%switch_install_vcpkg_path%\vcpkg" integrate install
-  call scripts\install\install_windows_generate_and_build.bat "" "-DCMAKE_TOOLCHAIN_FILE=%switch_install_vcpkg_path%/scripts/buildsystems/vcpkg.cmake"
-) else if "%switch_install_option%" == "/VS:2017:WIN64" (
-  call scripts\install\install_windows_generate_and_build.bat -G "Visual Studio 15 2017 Win64"
-) else if "%switch_install_option%" == "/VS:2017:WIN32" (
-  call scripts\install\install_windows_generate_and_build.bat -G "Visual Studio 15 2017"
-) else if "%switch_install_option%" == "/VS:2015:WIN64" (
-  call scripts\install\install_windows_generate_and_build.bat -G "Visual Studio 14 2015 Win64"
-) else if "%switch_install_option%" == "/VS:2015:WIN32" (
-  call scripts\install\install_windows_generate_and_build.bat -G "Visual Studio 14 2015"
-) else (
+if "%1" == "/help" (
   call scripts\install\install_windows_usage.bat 
-) 
+) else (
+devenv /?  2> NUL
+  if %ERRORLEVEL%==9009 (
+    echo Installation failed : Visual Studio not found!
+    echo.
+    echo Before using this install.bat file, be sure you are in "Developer Command Prompt for 
+    echo VS 2017" or "Developer Command Prompt for VS 2015" command box or be sure your path
+    echo environment variable contains the folder path containing "devenv.exe" program 
+    echo ^(if you do not change the default directory when installing it is 
+    echo "C:\Program Files\Visual Studio 15\Common7\IDE"; otherwise is your specified path + 
+    echo "\Common7\IDE" to terminate^).
+    echo.
+  ) else if "%switch_install_option%" == "/VCPKG" (
+    "%switch_install_vcpkg_path%\vcpkg" install curl gtest libjpeg-turbo zlib libpng
+    "%switch_install_vcpkg_path%\vcpkg" integrate install
+    call scripts\install\install_windows_generate_and_build.bat "" "-DCMAKE_TOOLCHAIN_FILE=%switch_install_vcpkg_path%/scripts/buildsystems/vcpkg.cmake"
+  ) else if "%switch_install_option%" == "/VS:2017:WIN64" (
+    call scripts\install\install_windows_generate_and_build.bat -G "Visual Studio 15 2017 Win64"
+  ) else if "%switch_install_option%" == "/VS:2017:WIN32" (
+    call scripts\install\install_windows_generate_and_build.bat -G "Visual Studio 15 2017"
+  ) else if "%switch_install_option%" == "/VS:2015:WIN64" (
+    call scripts\install\install_windows_generate_and_build.bat -G "Visual Studio 14 2015 Win64"
+  ) else if "%switch_install_option%" == "/VS:2015:WIN32" (
+    call scripts\install\install_windows_generate_and_build.bat -G "Visual Studio 14 2015"
+  ) else (
+    call scripts\install\install_windows_usage.bat 
+  ) 
+)
 
 set switch_install_option=
 set switch_install_vcpkg_path=
