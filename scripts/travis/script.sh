4e252133864883c7bd43baa4466ac2188ca46fa3
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-11-09 19:02:19 +0100

    travis script fix

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -2,19 +2,35 @@
 
 # generate, build and install 3rdparty
 cd build/3rdparty
-cmake -DCMAKE_BUILD_TYPE=Release ../../3rdparty 
-cmake --build . --target install -- -j8
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  cmake -DCMAKE_BUILD_TYPE=Release ../../3rdparty 
+  cmake --build . --target install -- -j8
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  cmake -G "Xcode" ../../3rdparty 
+  cmake --build . --target install --Config Release
+fi
 cd ../..
 
 # generate, build and install Switch
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON ..
-cmake --build . --target install -- -j8
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON .. 
+  cmake --build . --target install -- -j8
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  cmake -G "Xcode" -DBUILD_TESTS=ON .. 
+  cmake --build . --target install --Config Release
+fi
 cd ..
 
 # generate examples
 cd build/examples
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON ../../examples
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  cmake -DCMAKE_BUILD_TYPE=Release ../../examples 
+  cmake --build . --target install -- -j8
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  cmake -G "Xcode" ../../examples 
+  cmake --build . --target install --Config Release
+fi
 cd ../..
 
 # run unittests
