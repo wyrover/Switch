1cfa4b091cbb7c9e239c46bfd02bec7131b20bbe
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-11-10 15:07:06 +0100

    review cmake

--- scripts/cmake/SwitchConfig.cmake
+++ scripts/cmake/SwitchConfig.cmake
@@ -6,6 +6,15 @@ get_filename_component(Switch_LIBRARIES_DIRS "${CMAKE_CURRENT_LIST_DIR}/../lib" 
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
+find_package(call_stack REQUIRED)
+find_package(CURL REQUIRED)
+find_package(easywsclient REQUIRED)
+find_package(GTest REQUIRED)
+find_package(JPEG REQUIRED)
+find_package(PNG REQUIRED)
+find_package(Rs232 REQUIRED)
+find_package(ZLIB REQUIRED)
+
 include(${CMAKE_CURRENT_LIST_DIR}/SwitchDependencies.cmake)
 
 SET(Switch_LIBRARIES Switch.TUnit.Core Switch.System.Windows.Forms Switch.System.Drawing Switch.System Switch.Core)
