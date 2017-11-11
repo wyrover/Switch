ebcbb5f8386f4c55adb2b44cf8edcc6ff16990be
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-11-11 01:25:48 +0100

    fix dependence

--- scripts/cmake/SwitchConfig.cmake
+++ scripts/cmake/SwitchConfig.cmake
@@ -6,18 +6,17 @@ get_filename_component(Switch_LIBRARIES_DIRS "${CMAKE_CURRENT_LIST_DIR}/../lib" 
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-find_package(call_stack REQUIRED)
+#find_package(call_stack REQUIRED)
 find_package(CURL REQUIRED)
-find_package(easywsclient REQUIRED)
-find_package(GTest REQUIRED)
-find_package(JPEG REQUIRED)
-find_package(PNG REQUIRED)
-find_package(Rs232 REQUIRED)
-find_package(ZLIB REQUIRED)
+#find_package(easywsclient REQUIRED)
+#find_package(jpeg REQUIRED)
+#find_package(libpng REQUIRED)
+#find_package(Rs232 REQUIRED)
+#find_package(ZLIB REQUIRED)
 
 include(${CMAKE_CURRENT_LIST_DIR}/SwitchDependencies.cmake)
 
-SET(Switch_LIBRARIES Switch.TUnit.Core Switch.System.Windows.Forms Switch.System.Drawing Switch.System Switch.Core)
+SET(Switch_LIBRARIES Switch.TUnit.Core Switch.System.Windows.Forms Switch.System.Drawing Switch.System Switch.Core call_stack easywsclient jpeg libpng Rs232 ZLIB)
 SET(Switch_WITH_TUNIT_MAIN_LIBRARIES Switch.TUnit.Main ${Switch_LIBRARIES})
 
 set(Switch_FOUND TRUE)
