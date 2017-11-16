a17bef404bac4ddef2c879e06a5e736ec9946311
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-11-17 00:06:20 +0100

    build Switch 32 and 64 bits

--- scripts/appveyor/test_script.bat
+++ scripts/appveyor/test_script.bat
@@ -1,4 +1,9 @@
 rem run registered unit tests
-cd build
+cd build32
+ctest --output-on-failure --build-config Release
+cd ..
+
+rem run registered unit tests
+cd build64
 ctest --output-on-failure --build-config Release
 cd ..
