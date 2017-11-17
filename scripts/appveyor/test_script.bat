64c60fe5b15bebacd66990b1171ed5a0e223d78d
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-11-17 11:31:13 +0100

    build 64 bits only

--- scripts/appveyor/test_script.bat
+++ scripts/appveyor/test_script.bat
@@ -1,9 +1,13 @@
-rem run registered unit tests
-cd build32
-ctest --output-on-failure --build-config Release
-cd ..
+rem run registered unit tests 32 bits
+if "%SWITCH_BUILD_WIN32%" == "true" (
+  cd build32
+  ctest --output-on-failure --build-config Release
+  cd ..
+)
 
-rem run registered unit tests
-cd build64
-ctest --output-on-failure --build-config Release
-cd ..
+rem run registered unit tests 64 bits
+if "%SWITCH_BUILD_WIN64%" == "true" (
+  cd build64
+  ctest --output-on-failure --build-config Release
+  cd ..
+)
