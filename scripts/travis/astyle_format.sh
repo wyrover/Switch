d422bfb5a98f2cefbf789eb1a0b3c8a3e502fb71
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-11-23 09:41:43 +0100

    fix astyle format script

--- scripts/travis/astyle_format.sh
+++ scripts/travis/astyle_format.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+set -ev
+
 # generate, build and install 3rdparty
 cd build/3rdparty
 cmake ../../3rdparty -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/usr/local -DCMAKE_CXX_COMPILER=clang++-3.9
