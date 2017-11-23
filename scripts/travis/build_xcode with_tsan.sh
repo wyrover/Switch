7f5cf6fd9fd3c94663a3001355c895443809d272
gammasoft71 <gammasoft71@gmail.com>
2017-11-24 00:36:00 +0100

    add Google Address Sanitizer, add Google Undefined Sanitizer and add Google Thread Sanitizer

---
+++ scripts/travis/build_xcode with_tsan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ev
+
+# generate, build and install 3rdparty
+cd build/3rdparty
+cmake ../../3rdparty -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/usr/local
+cmake --build . --target install -- -j8
+cd ../..
+
+# generate, build and install Switch
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DENABLE_TSAN=ON -DCMAKE_INSTALL_PREFIX=~/usr/local
+cmake --build . -- -j8
+cd ..
+
+# run registered unit tests
+cd build
+ctest --output-on-failure --build-config Release
+cd ..
