30ceb7e54db7aef897bf06813d7445515be3bf82
gammasoft71 <gammasoft71@gmail.com>
2017-11-23 14:10:42 +0100

    remove log from codecov

--- scripts/travis/publish_to_codecov.sh
+++ scripts/travis/publish_to_codecov.sh
@@ -4,4 +4,4 @@ set -ev
 
 # publish to codecov
 export CODECOV_TOKEN="6f14306e-b89d-4ee1-a370-4549f15a14b6"
-bash <(curl -s https://codecov.io/bash)
+bash <(curl -s https://codecov.io/bash) > /dev/null 2>&1
