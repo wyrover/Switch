aa3de8bba86d84e128d26d3250d0dd13d0c8f6de
gammasoft71 <gammasoft71@gmail.com>
2017-11-23 20:52:18 +0100

    add publish to coverall

---
+++ scripts/travis/publish_to_coverall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ev
+
+# install cpp-coverall
+brew update
+brew install pyenv
+eval "$(pyenv init -)"
+pyenv install 2.7.6
+pyenv global 2.7.6
+pyenv rehash
+pip install cpp-coveralls
+pyenv rehash
+
+# publish to coverall
+coveralls --build-root build --gcov-options '\-lp' -e build/3rdparty -e build/astyle-prefix -e build/cppcheck-prefix -e build/examples -e build/ReferenceGuide -e build/include -e build/CMakeFiles/3.9.4 -e build/CMakeFiles/feature_tests.c -e build/CMakeFiles/feature_tests.cxx
