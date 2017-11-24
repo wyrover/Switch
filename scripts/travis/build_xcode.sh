a16b237bb8fd817362bf491379b5c7e4f9d2ea33
gammasoft71 <gammasoft71@gmail.com>
2017-11-25 00:21:18 +0100

    fix scripts

--- scripts/travis/build_xcode.sh
+++ scripts/travis/build_xcode.sh
@@ -10,7 +10,7 @@ cd ../..
 
 # generate, build and install Switch
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DENABLE_COVERAGE=ON -DCMAKE_INSTALL_PREFIX=~/usr/local
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DENABLE_COVERAGE=OFF -DCMAKE_INSTALL_PREFIX=~/usr/local
 cmake --build . -- -j8
 cd ..
 
