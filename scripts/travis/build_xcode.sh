d5ea2af75185432bcca79e2da4bd088754ac0021
gammasoft71 <gammasoft71@gmail.com>
2017-11-25 01:22:00 +0100

    coverage disabled if not the job exceeds the maximum time limit

--- scripts/travis/build_xcode.sh
+++ scripts/travis/build_xcode.sh
@@ -10,6 +10,7 @@ cd ../..
 
 # generate, build and install Switch
 cd build
+# ENABLE_COVERAGE deactivated if not the job exceeds the maximum time limit.
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DENABLE_COVERAGE=OFF -DCMAKE_INSTALL_PREFIX=~/usr/local
 cmake --build . -- -j8
 cd ..
--- scripts/travis/publish_to_codecov.sh
+++ scripts/travis/publish_to_codecov.sh
@@ -4,4 +4,5 @@ set -ev
 
 # publish to codecov
 export CODECOV_TOKEN="6f14306e-b89d-4ee1-a370-4549f15a14b6"
-bash <(curl -s https://codecov.io/bash) > /dev/null 2>&1
+# codecov deactivate if not the job exceeds the maximum time limit.
+#bash <(curl -s https://codecov.io/bash) > /dev/null 2>&1
