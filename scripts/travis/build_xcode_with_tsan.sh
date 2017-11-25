70f998a093479be265a594220d72b57d38b616cd
gammasoft71 <gammasoft71@gmail.com>
2017-11-25 09:46:34 +0100

    Google thread sanitizer disabled if not the job exceeds the maximum time limit

--- scripts/travis/build_xcode_with_tsan.sh
+++ scripts/travis/build_xcode_with_tsan.sh
@@ -10,7 +10,8 @@ cd ../..
 
 # generate, build and install Switch
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DENABLE_TSAN=ON -DCMAKE_INSTALL_PREFIX=~/usr/local
+#cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DENABLE_TSAN=ON -DCMAKE_INSTALL_PREFIX=~/usr/local
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DENABLE_TSAN=OFF -DCMAKE_INSTALL_PREFIX=~/usr/local
 cmake --build . -- -j8
 cd ..
 
--- scripts/travis/publish_to_codecov.sh
+++ scripts/travis/publish_to_codecov.sh
@@ -4,5 +4,5 @@ set -ev
 
 # publish to codecov
 export CODECOV_TOKEN="6f14306e-b89d-4ee1-a370-4549f15a14b6"
-# codecov deactivate if not the job exceeds the maximum time limit.
+# codecov deactivated if not the job exceeds the maximum time limit.
 #bash <(curl -s https://codecov.io/bash) > /dev/null 2>&1
