0a8723dda18124f546a455e049bb4119e4a2490f
gammasoft71 <gammasoft71@gmail.com>
2017-11-27 17:23:37 +0100

    fix settings

--- scripts/cmake/SwitchConfig.cmake
+++ scripts/cmake/SwitchConfig.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/Switch.cmake)  
 
-get_filename_component(Switch_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../include" ABSOLUTE)
-get_filename_component(Switch_LIBRARIES_DIRS "${CMAKE_CURRENT_LIST_DIR}/../lib" ABSOLUTE)
+get_filename_component(SWITCH_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../include" ABSOLUTE)
+get_filename_component(SWITCH_LIBRARIES_DIRS "${CMAKE_CURRENT_LIST_DIR}/../lib" ABSOLUTE)
 
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
@@ -18,8 +18,7 @@ find_package(ZLIB REQUIRED)
 
 include(${CMAKE_CURRENT_LIST_DIR}/SwitchDependencies.cmake)
 
-SET(Switch_LIBRARIES Switch.TUnit.Core Switch.System.Windows.Forms Switch.System.Drawing Switch.System Switch.Core)
-SET(Switch_WITH_TUNIT_MAIN_LIBRARIES Switch.TUnit.Main ${Switch_LIBRARIES})
+SET(SWITCH_LIBRARIES Switch.TUnit.Core Switch.System.Windows.Forms Switch.System.Drawing Switch.System Switch.Core)
+SET(SWITCH_WITH_TUNIT_MAIN_LIBRARIES Switch.TUnit.Main ${Switch_LIBRARIES})
 
-set(Switch_FOUND TRUE)
 set(SWITCH_FOUND TRUE)
