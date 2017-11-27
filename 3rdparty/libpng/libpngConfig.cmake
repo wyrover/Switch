26e20331f2fc1101bbe64dbc3179f9ac4d9c2832
gammasoft71 <gammasoft71@gmail.com>
2017-11-27 17:52:44 +0100

    change libpng to v1.6.34 from git

--- 3rdparty/libpng/libpngConfig.cmake
+++
@@ -1,9 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/libpng.cmake)  
-
-get_filename_component(PNG_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../include" ABSOLUTE)
-get_filename_component(PNG_LIBRARIES_DIRS "${CMAKE_CURRENT_LIST_DIR}/../lib" ABSOLUTE)
-
-SET(PNG_LIBRARIES libpng)
-
-set(png_FOUND TRUE)
-set(PNG_FOUND TRUE)
