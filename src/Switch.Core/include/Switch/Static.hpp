e477c9cb8910a83535b37f69a1ef2325dc3b3c98
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-12-03 10:29:51 +0100

    Fix format

--- src/Switch.Core/include/Switch/Static.hpp
+++ src/Switch.Core/include/Switch/Static.hpp
@@ -15,18 +15,18 @@ namespace Switch {
     Static& operator=(const Static&) = delete;
     /// @endcond
   };
-  
-  /// @brief This keyword is use to represent a static class. A static class can't be instancied (constructors are deleted).
-  /// @par Examples
-  /// @code
-  /// class Foo _static {
-  /// public:
-  ///   static string ToString() {return "Foo";}
-  /// };
-  /// @endcode
-  /// @ingroup Keywords
-#define _static \
-final : Static
 }
+
+/// @brief This keyword is use to represent a static class. A static class can't be instancied (constructors are deleted).
+/// @par Examples
+/// @code
+/// class Foo _static {
+/// public:
+///   static string ToString() {return "Foo";}
+/// };
+/// @endcode
+/// @ingroup Keywords
+#define _static \
+  final : Static
 
 using namespace Switch;
