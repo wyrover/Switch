ecf3e8842db0f4735ad0a664292c71a9e71bfdc5
gammasoft71 <gammasoft71@gmail.com>
2017-12-04 07:33:53 +0100

    fix gitcheck

--- src/Switch/include/Switch/Switch.hpp
+++ src/Switch/include/Switch/Switch.hpp
@@ -1,16 +1,9 @@
 #pragma once
 
 #include "Switch/Switch.Core"
-#include "Switch/Switch.Core"
-#include "Switch/Switch.System"
 #include "Switch/Switch.System"
 #include "Switch/Switch.System.Core"
-#include "Switch/Switch.System.Core"
-#include "Switch/Switch.System.Drawing"
 #include "Switch/Switch.System.Drawing"
 #include "Switch/Switch.System.ServiceModel"
-#include "Switch/Switch.System.ServiceModel"
 #include "Switch/Switch.System.Windows.Forms"
-#include "Switch/Switch.System.Windows.Forms"
-#include "Switch/Switch.TUnit.Core"
 #include "Switch/Switch.TUnit.Core"
