92d8c77df2985133c41d1dc051025baf49ea4a78
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-12-28 16:17:30 +0100

    add MINGW64

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -3,16 +3,17 @@ echo ""
 
 # Detecting linux distribution
 OSTYPE=`uname -a`
-if [[ "$OSTYPE" != *"Darwin"* ]]; then OSTYPE=`lsb_release -si`; fi
+if [[ "$OSTYPE" != *"Darwin"* ]] && [[ "$OSTYPE" != *"MINGW64"* ]]; then OSTYPE=`lsb_release -si`; fi
 
 # install needed packages and libraries for known distribution
 case "$OSTYPE" in
   *"CentOS"*) sudo yum update; sudo yum install clang cmake3 doxygen gtkmm30-devel uuid-devel -y;;
-  *"Darwin"*) sudo chown -R $(whoami) /usr/local/share; brew update; brew install cmake doxygen;;
+  *"Darwin"*) sudo chown -R $(whoami) /usr/local/share; brew update; brew install astyle cmake cppcheck doxygen;;
   *"Debian"*) sudo apt update; sudo apt install clang cmake doxygen libgtkmm-3.0-dev libssl-dev uuid-dev -y;;
   *"elementary"*) sudo apt update; sudo apt install clang cmake doxygen libgtkmm-3.0-dev libssl-dev uuid-dev -y;;
   *"Fedora"*) sudo yum update; sudo yum install clang cmake doxygen gtkmm30-devel libuuid-devel uuid-devel -y;;
   *"LinuxMint"*) sudo apt update; sudo apt install clang cmake doxygen libgtkmm-3.0-dev libssl-dev uuid-dev -y;;
+  *"MINGW64"*) ;;
   *"RedHat"*) sudo yum update; sudo yum install clang cmake doxygen gtkmm30-devel libuuid-devel uuid-devel -y;;
   *"Ubuntu"*) sudo apt update; sudo apt install clang cmake doxygen libgtkmm-3.0-dev libssl-dev uuid-dev -y;;
 esac
@@ -26,21 +27,34 @@ mkdir -p build/examples
 
 # generate, build and install Switch
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug "$@"
-cmake --build . -- -j8
-sudo cmake --build . --target install
-cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . -- -j8
-sudo cmake --build . --target install
+if [[ "$OSTYPE" == *"Darwin"* ]]; then
+  cmake .. -G "Xcode" "$@"
+  cmake --build . --target install --config Debug
+  cmake --build . --target install --config Release
+elif [[ "$OSTYPE" == *"MINGW64"* ]]; then
+  cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local "$@"
+  cmake --build . --target install --config Debug
+  cmake --build . --target install --config Release
+else
+  cmake .. -DCMAKE_BUILD_TYPE=Debug "$@"
+  cmake --build . -- -j8
+  sudo cmake --build . --target install
+  cmake .. -DCMAKE_BUILD_TYPE=Release "$@"
+  cmake --build . -- -j8
+  sudo cmake --build . --target install
+fi
 cd ..
 
 # generate and launch examples
 cd build/examples
-if [[ "$OSTYPE" == *"Darwin"* ]]; then 
+if [[ "$OSTYPE" == *"Darwin"* ]]; then
   cmake ../../examples -G "Xcode" "$@"
   open Examples.xcodeproj
+elif [[ "$OSTYPE" == *"MINGW64"* ]]; then
+  cmake ../../examples -DCMAKE_INSTALL_PREFIX=/usr/local "$@"
+  start Examples.sln
 else
-  cmake ../../examples -DCMAKE_BUILD_TYPE=Debug "$@"
+  cmake ../../examples -DCMAKE_BUILD_TYPE=Debug  "$@"
   cd build/examples
   echo 
   echo You can now build and execute examples.
