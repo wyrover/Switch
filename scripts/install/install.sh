aca5102beff88705b833e1be63247a97de26817a
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2017-12-29 18:41:04 +0100

    fix mingw

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -1,11 +1,17 @@
 echo "Install Switch libraries version $switch_version, copyright GAMMA Soft, 2017"
 echo ""
 
-# Detecting linux distribution
-OSTYPE=`uname -a`
-if [[ "$OSTYPE" != *"Darwin"* ]] && [[ "$OSTYPE" != *"MINGW64"* ]]; then OSTYPE=`lsb_release -si`; fi
+# ________________________________________________________________________________________
+#                                                             Detecting linux distribution
 
-# install needed packages and libraries for known distribution
+OSTYPE=`uname -a`
+if [[ "$OSTYPE" != *"Darwin"* ]] && [[ "$OSTYPE" != *"MINGW64"* ]]; then
+  OSTYPE=`lsb_release -si`;
+fi
+
+# ________________________________________________________________________________________
+#                             install needed packages and libraries for known distribution
+
 case "$OSTYPE" in
   *"CentOS"*) sudo yum update; sudo yum install clang cmake3 doxygen gtkmm30-devel uuid-devel -y;;
   *"Darwin"*) sudo chown -R $(whoami) /usr/local/share; brew update; brew install astyle cmake cppcheck doxygen;;
@@ -18,14 +24,22 @@ case "$OSTYPE" in
   *"Ubuntu"*) sudo apt update; sudo apt install clang cmake doxygen libgtkmm-3.0-dev libssl-dev uuid-dev -y;;
 esac
 
-# set clang as default compiler
-export CC=clang
-export CXX=clang++
+# ________________________________________________________________________________________
+#                                                            set clang as default compiler
 
-# create build folders
+if [[ "$OSTYPE" != *"Darwin"* ]] && [[ "$OSTYPE" != *"MINGW64"* ]]; then
+  export CC=clang
+  export CXX=clang++
+fi
+
+# ________________________________________________________________________________________
+#                                                                     create build folders
+
 mkdir -p build/examples
 
-# generate, build and install Switch
+# ________________________________________________________________________________________
+#                                                       generate, build and install Switch
+
 cd build
 if [[ "$OSTYPE" == *"Darwin"* ]]; then
   cmake .. -G "Xcode" "$@"
@@ -45,7 +59,9 @@ else
 fi
 cd ..
 
-# generate and launch examples
+# ________________________________________________________________________________________
+#                                                             generate and launch examples
+
 cd build/examples
 if [[ "$OSTYPE" == *"Darwin"* ]]; then
   cmake ../../examples -G "Xcode" "$@"
