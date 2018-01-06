5ce046568110d5aa5e502a1da682fa2472b81e8e
gammasoft71 <gammasoft71@gmail.com>
2018-01-06 21:15:25 +0100

    add comments

--- scripts/cmake/AddCustomTargets.cmake
+++ scripts/cmake/AddCustomTargets.cmake
@@ -1,8 +1,8 @@
 macro(AddCustomTargets)
   print("Custom Targets:")
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                           Switch.All
+  #_______________________________________________________________________________________
+  #                                                                             Switch.All
   print("  [X] ${PROJECT_NAME}.All")
   if (WIN32)
     add_custom_target(${PROJECT_NAME}.All "${CMAKE_COMMAND}" --build "${CMAKE_BINARY_DIR}" --config ${SWITCH_BUILD_TYPE} DEPENDS ${PROJECT_NAME} COMMENT "Installing ${PROJECT_NAME}")
@@ -12,24 +12,24 @@ macro(AddCustomTargets)
     add_custom_target(${PROJECT_NAME}.All "${CMAKE_COMMAND}" --build "${CMAKE_BINARY_DIR}" -- -j 8 DEPENDS ${PROJECT_NAME} COMMENT "Installing ${PROJECT_NAME}")
   endif()
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                         Switch.Check
+  #_______________________________________________________________________________________
+  #                                                                           Switch.Check
   print("  [X] ${PROJECT_NAME}.Check")
   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/.cppcheck_false_positive.txt ${CMAKE_CURRENT_BINARY_DIR}/cppcheck_false_positive.txt @ONLY)
   add_custom_target(${PROJECT_NAME}.Check COMMAND ${CPPCHECK_EXECUTABLE} ${CPPCHECK_ARGS} COMMENT "running cppcheck" DEPENDS ${CPPCHECK_PROJECT})
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                        Switch.Format
+  #_______________________________________________________________________________________
+  #                                                                          Switch.Format
   print("  [X] ${PROJECT_NAME}.Format")
   add_custom_target(${PROJECT_NAME}.Format COMMAND ${ASTYLE_EXECUTABLE} ${ASTYLE_ARGS} COMMENT "running astyle" DEPENDS ${ASTYLE_PROJECT})
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                      Switch.Gitcheck
+  #_______________________________________________________________________________________
+  #                                                                        Switch.Gitcheck
   print("  [X] ${PROJECT_NAME}.GitCheck")
   add_custom_target(${PROJECT_NAME}.GitCheck COMMAND git diff --check HEAD^ COMMENT "running git check")
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                       Switch.Install
+  #_______________________________________________________________________________________
+  #                                                                         Switch.Install
   print("  [X] ${PROJECT_NAME}.Install")
   if (WIN32)
     add_custom_target(${PROJECT_NAME}.Install "${CMAKE_COMMAND}" --build "${CMAKE_BINARY_DIR}" --target install --config ${SWITCH_BUILD_TYPE} DEPENDS ${PROJECT_NAME} COMMENT "Installing ${PROJECT_NAME}")
@@ -39,15 +39,15 @@ macro(AddCustomTargets)
     add_custom_target(${PROJECT_NAME}.Install "${CMAKE_COMMAND}" --build "${CMAKE_BINARY_DIR}" --target install -- -j 8 DEPENDS ${PROJECT_NAME} COMMENT "Installing ${PROJECT_NAME}")
   endif()
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                Switch.ReferenceGuide
+  #_______________________________________________________________________________________
+  #                                                                  Switch.ReferenceGuide
   print("  [X] ${PROJECT_NAME}.ReferenceGuide")
   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/.doxygen.txt ${CMAKE_CURRENT_BINARY_DIR}/doxygen.txt @ONLY)
   file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/ReferenceGuide)
   add_custom_target(${PROJECT_NAME}.ReferenceGuide ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/doxygen.txt WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/ReferenceGuide COMMENT "Doxygen Reference Guide generation" DEPENDS ${DOXYGEN_PROJECT} VERBATIM)
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                         Switch.Tests
+  #_______________________________________________________________________________________
+  #                                                                           Switch.Tests
   print("  [X] ${PROJECT_NAME}.Tests")
   add_custom_target(${PROJECT_NAME}.Tests COMMAND ctest --output-on-failure -C ${SWITCH_BUILD_TYPE} DEPENDS ${PROJECT_NAME}.All)
 endmacro()
--- scripts/cmake/DownloadTools.cmake
+++ scripts/cmake/DownloadTools.cmake
@@ -1,8 +1,8 @@
 macro(DownloadTools)
   print("Tools:")
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                                  Git
+  #_______________________________________________________________________________________
+  #                                                                                    Git
   find_package(Git REQUIRED QUIET)
   if (Git_FOUND)
     print("  [X] Git ${GIT_VERSION_STRING}")
@@ -10,8 +10,8 @@ macro(DownloadTools)
     print("  [ ] Git")
   endif()
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                               astyle
+  #_______________________________________________________________________________________
+  #                                                                                 astyle
   if (DOWNLOAD_ASTYLE)
     print("  [X] astyle 3.0.1 (*)")
     ExternalProject_Add(astyle GIT_REPOSITORY https://github.com/Bareflank/astyle.git GIT_TAG v1.2 GIT_SHALLOW 1 CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR})
@@ -22,8 +22,8 @@ macro(DownloadTools)
     print("  [ ] astyle")
   endif()
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                             cppcheck
+  #_______________________________________________________________________________________
+  #                                                                               cppcheck
   if (DOWNLOAD_CPPCHECK)
     print("  [X] cppcheck 1.81 (*)")
     ExternalProject_Add(cppcheck GIT_REPOSITORY https://github.com/danmar/cppcheck.git GIT_TAG 1.81 GIT_SHALLOW 1 CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR})
@@ -34,8 +34,8 @@ macro(DownloadTools)
     print("  [ ] cppcheck")
   endif()
 
-  #_____________________________________________________________________________________________________________________
-  #                                                                                                              doxygen
+  #_______________________________________________________________________________________
+  #                                                                                Doxygen
   find_package(Doxygen QUIET)
   if (Doxygen_FOUND)
     print("  [X] Doxygen ${DOXYGEN_VERSION}")
