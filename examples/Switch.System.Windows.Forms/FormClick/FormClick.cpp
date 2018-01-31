4ae0eb618ca060b88e5c515db44ebd044bbffccf
gammasoft71 <gammasoft71@gmail.com>
2018-01-31 15:41:00 +0100

    fix format

--- examples/Switch.System.Windows.Forms/FormClick/FormClick.cpp
+++ examples/Switch.System.Windows.Forms/FormClick/FormClick.cpp
@@ -13,14 +13,14 @@ namespace FormClickExample {
 
       Array<Color> colors = {Color::Pink, Color::LightBlue, Color::LightGreen, Color::LightYellow};
       int colorIndex = 0;;
- 
+
       Form form;
       form.BackColor = colors[colorIndex];
       form.Click += delegate_(const object & sender, const EventArgs & e) {
         if (++colorIndex == colors.Length) colorIndex = 0;
         form.BackColor = colors[colorIndex];
       };
-      
+
       Application::Run(form);
     }
   };
